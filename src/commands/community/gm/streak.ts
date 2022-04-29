5b38fd54297d25bfa7e35da2d253e3769f2bc02f
Phuong <phuwn29@gmail.com>
2022-04-29 15:11:44 +0700

    chore: change streak message to embeds

--- src/commands/community/gm/streak.ts
+++ src/commands/community/gm/streak.ts
@@ -2,33 +2,41 @@ import { logger } from "logger"
 import { Command } from "types/common"
 import { PREFIX } from "utils/constants"
 import { composeEmbedMessage } from "utils/discordEmbed"
+import { ColorResolvable, MessageEmbed } from "discord.js"
+import { msgColors } from "utils/common"
 import Profile from "../../../adapters/profile"
 
 const command: Command = {
-  id: "gm-streak",
+  id: "gm_streak",
   command: "streak",
   brief: "Show user's gm/gn streak",
   category: "Community",
   run: async msg => {
     try {
       const json = await Profile.getUserGmStreak(msg.author.id, msg.guildId)
+      let daysCheckedIcons = ""
 
       switch (json.error) {
         case "user has no gm streak":
           break
         case undefined:
+          for (let i = 0; i < json.data.streak_count; i++) {
+            daysCheckedIcons += "<:approve:933341948402618378>"
+          }
           return {
             messageOptions: {
-              content: `gm <@${msg.author.id}>, you've said gm-gn ${
-                json.data.streak_count
-              } day${
-                json.data.streak_count > 1 ? "s" : ""
-              } in a row :fire: and ${json.data.total_count} day${
-                json.data.total_count > 1 ? "s" : ""
-              } in total.`
+              embeds: [
+                new MessageEmbed()
+                  .setTitle(`GM/GN streak`)
+                  .setDescription(
+                    `GM streak: **${json.data.streak_count}**
+                    ${daysCheckedIcons}
+                    `
+                  )
+                  .setColor(msgColors.PRIMARY as ColorResolvable)
+              ]
             }
           }
-          break
         default:
           throw new Error(json.error)
       }
@@ -37,13 +45,14 @@ const command: Command = {
     }
   },
   getHelpMessage: async msg => {
-    const embed = composeEmbedMessage(msg, {
-      description: "Show user's gm/gn streak",
-      usage: `${PREFIX}gm streak`,
-      examples: `${PREFIX}gm streak`
-    })
     return {
-      embeds: [embed]
+      embeds: [
+        composeEmbedMessage(msg, {
+          description: "Show user's gm/gn streak",
+          usage: `${PREFIX}gm streak`,
+          examples: `${PREFIX}gm streak`
+        })
+      ]
     }
   },
   canRunWithoutAction: true
