378ebb4125d4b97dc6ee02d43837b9914f87822a
anhnh12 <nguyenhoanganh0772@gmail.com>
2022-05-12 18:24:37 +0700

    feat: top users command

--- src/adapters/community.ts
+++ src/adapters/community.ts
@@ -1,9 +1,11 @@
+import { Message } from "discord.js"
+import { BotBaseError } from "errors"
 import { logger } from "logger"
 import fetch from "node-fetch"
 import { InvitesInput } from "types/community"
 import { API_BASE_URL } from "utils/constants"
 
-class Community {  
+class Community {
   public async getInvites(input: InvitesInput): Promise<any> {
     try {
       const res = await fetch(
@@ -15,8 +17,8 @@ class Community {
       logger.error(e)
     }
   }
-  
-  public async getInvitesLeaderboard(guildId: string ): Promise<any> {
+
+  public async getInvitesLeaderboard(guildId: string): Promise<any> {
     try {
       const res = await fetch(
         `${API_BASE_URL}/community/invites/leaderboard/${guildId}`
@@ -39,25 +41,25 @@ class Community {
       logger.error(e)
     }
   }
-  
-  public async configureInvites(body: string ): Promise<any> {
+
+  public async configureInvites(body: string): Promise<any> {
     try {
-      const res = await fetch(
-        `${API_BASE_URL}/community/invites/config`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body,
-        },
-      )
+      const res = await fetch(`${API_BASE_URL}/community/invites/config`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body
+      })
 
       return await res.json()
     } catch (e: any) {
       logger.error(e)
     }
   }
-  
-  public async getUserInvitesAggregation(guildId: string, inviterId: string ): Promise<any> {
+
+  public async getUserInvitesAggregation(
+    guildId: string,
+    inviterId: string
+  ): Promise<any> {
     try {
       const res = await fetch(
         `${API_BASE_URL}/community/invites/aggregation?guild_id=${guildId}&inviter_id=${inviterId}`
@@ -67,6 +69,24 @@ class Community {
     } catch (e: any) {
       logger.error(e)
     }
+  }
+
+  public async getTopXPUsers(msg: Message, page: number): Promise<any> {
+    const resp = await fetch(
+      `${API_BASE_URL}/users/top?guild_id=${msg.guildId}&user_id=${msg.author.id}&page=${page}`,
+      {
+        method: "GET"
+      }
+    )
+    if (resp.status !== 200) {
+      throw new BotBaseError(msg)
+    }
+
+    const json = await resp.json()
+    if (json.error !== undefined) {
+      throw new Error(json.error)
+    }
+    return json.data
   }
 }
 
---
+++ src/commands/community/top.ts
@@ -0,0 +1,94 @@
+import { Command } from "types/common"
+import { Message, MessageAttachment } from "discord.js"
+import { DOT, PREFIX } from "utils/constants"
+import { composeEmbedMessage } from "utils/discordEmbed"
+import Community from "adapters/community"
+import * as Canvas from "canvas"
+import { getCommandArguments } from "utils/commands"
+
+async function renderLeaderboard(leaderboard: any[]) {
+  const canvas = Canvas.createCanvas(650, 600)
+  const ctx = canvas.getContext("2d")
+  ctx.fillStyle = "rgba(0, 0, 0, 0.2)"
+  ctx.fillRect(0, 0, 650, 600)
+
+  ctx.fillStyle = "white"
+  ctx.font = "bold 25px Manrope"
+  const row = {
+    x: 40,
+    y: 50
+  }
+  let { x, y } = row
+  for (const record of leaderboard) {
+    let { username } = record.user
+    username = username.length > 15 ? `${username.slice(0, 15)}...` : username
+    const userRank = `#${record.guild_rank} ${DOT} ${username}`
+    ctx.fillText(userRank, x, y)
+    x += 360
+    ctx.fillText(`lv.${record.level}`, x, y)
+    x += ctx.measureText(record.level).width + 80
+    ctx.fillText(`${record.total_xp} pts`, x, y)
+    x = row.x
+    y +=
+      ctx.measureText(record.total_xp).actualBoundingBoxAscent +
+      ctx.measureText(record.total_xp).actualBoundingBoxDescent +
+      40
+  }
+  return new MessageAttachment(canvas.toBuffer(), "leaderboard.png")
+}
+
+const command: Command = {
+  id: "top",
+  command: "top",
+  brief: "All-time Rankings",
+  category: "Community",
+  run: async function(msg: Message) {
+    const args = getCommandArguments(msg)
+    let page = args.length > 1 ? +args[1] : 0
+    page = Math.min(isNaN(page) ? 0 : page - 1, 0)
+    const data = await Community.getTopXPUsers(msg, page)
+    if (!data || !data.leaderboard || !data.leaderboard.length)
+      return {
+        messageOptions: {
+          embeds: [
+            composeEmbedMessage(msg, {
+              title: msg.guild.name,
+              description: "No ranking data found"
+            })
+          ]
+        }
+      }
+
+    const { author, leaderboard } = data
+    let description = "**Your rank**\n"
+    if (author.guild_rank) {
+      description += `You are rank \`#${author.guild_rank}\` on this server\nwith a total of **${author.total_xp} XP score**`
+    } else {
+      description += "You are `unranked` on this server"
+    }
+    const embed = composeEmbedMessage(msg, {
+      title: `${msg.guild.name}'s all-time rankings`,
+      thumbnail: msg.guild.iconURL(),
+      description,
+      image: "attachment://leaderboard.png"
+    })
+
+    return {
+      messageOptions: {
+        embeds: [embed],
+        files: [await renderLeaderboard(leaderboard)]
+      }
+    }
+  },
+  getHelpMessage: async msg => ({
+    embeds: [
+      composeEmbedMessage(msg, {
+        usage: `${PREFIX}top`,
+        examples: `${PREFIX}top`
+      })
+    ]
+  }),
+  canRunWithoutAction: true
+}
+
+export default command
