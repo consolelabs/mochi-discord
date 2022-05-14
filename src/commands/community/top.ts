ac48eed4a7c388836b286f5a67f6ea316fe8c557
anhnh12 <nguyenhoanganh0772@gmail.com>
2022-05-14 18:41:46 +0700

    chore: user leaderboard canvas UI

--- src/commands/community/top.ts
+++ src/commands/community/top.ts
@@ -6,34 +6,97 @@ import Community from "adapters/community"
 import * as Canvas from "canvas"
 import { getCommandArguments } from "utils/commands"
 
+const getFont = (fontSize: number) => `bold ${fontSize}px Manrope`
+
+function adjustFontSize(
+  ctx: Canvas.CanvasRenderingContext2D,
+  str: string,
+  maxWidth: number,
+  fontSize: number
+) {
+  ctx.font = getFont(fontSize)
+  // minimum font size - 15
+  if (ctx.measureText(str).width >= maxWidth && fontSize >= 15) {
+    adjustFontSize(ctx, str, maxWidth, fontSize - 1)
+    return
+  }
+  ctx.font = getFont(fontSize)
+}
+
+function setRowTextColor(record: any, ctx: Canvas.CanvasRenderingContext2D) {
+  let color
+  switch (record.guild_rank) {
+    case 1:
+      color = "#FEE101"
+      break
+    case 2:
+      color = "#A7A7AD"
+      break
+    case 3:
+      color = "#A77044"
+      break
+    default:
+      color = "white"
+      break
+  }
+  ctx.fillStyle = color
+}
+
 async function renderLeaderboard(leaderboard: any[]) {
-  const canvas = Canvas.createCanvas(650, 600)
+  const containerWidth = 700
+  const containerHeight = 600
+  const canvas = Canvas.createCanvas(containerWidth, containerHeight)
   const ctx = canvas.getContext("2d")
   ctx.fillStyle = "rgba(0, 0, 0, 0.2)"
-  ctx.fillRect(0, 0, 650, 600)
+  ctx.fillRect(0, 0, containerWidth, containerHeight)
+  const fontSize = 25
+  ctx.font = getFont(fontSize)
 
-  ctx.fillStyle = "white"
-  ctx.font = "bold 25px Manrope"
   const row = {
-    x: 40,
-    y: 50
+    x: 0,
+    y: 0,
+    paddingLeft: 20,
+    paddingTop: 45
   }
-  let { x, y } = row
-  for (const record of leaderboard) {
-    let { username } = record.user
-    username = username.length > 15 ? `${username.slice(0, 15)}...` : username
-    const userRank = `#${record.guild_rank} ${DOT} ${username}`
-    ctx.fillText(userRank, x, y)
-    x += 360
-    ctx.fillText(`lv.${record.level}`, x, y)
-    x += ctx.measureText(record.level).width + 80
-    ctx.fillText(`${record.total_xp} pts`, x, y)
-    x = row.x
-    y +=
-      ctx.measureText(record.total_xp).actualBoundingBoxAscent +
-      ctx.measureText(record.total_xp).actualBoundingBoxDescent +
-      40
-  }
+  row.y += row.paddingTop
+  const rowGap = 35
+  const rankMaxWidth = ctx.measureText(
+    `#${leaderboard.slice(-1)[0].guild_rank} `
+  ).width
+  leaderboard.forEach(record => {
+    let x = row.x + row.paddingLeft
+    const rank = `#${record.guild_rank} `
+    const dot = `${DOT}  `
+    const username = record.user.username
+    const level = `lv.${record.level}`
+    const score = `${record.total_xp} pts`
+
+    // rank
+    setRowTextColor(record, ctx)
+    ctx.fillText(rank, x, row.y)
+    x += rankMaxWidth
+    // seperator
+    ctx.fillText(dot, x, row.y)
+    x += ctx.measureText(dot).width
+    // username
+    const usernameMaxWidth = 400
+    adjustFontSize(ctx, username, usernameMaxWidth, fontSize)
+    ctx.fillText(username, x, row.y)
+    x += usernameMaxWidth
+    // level
+    ctx.font = getFont(fontSize)
+    ctx.fillText(level, x, row.y)
+    x += ctx.measureText(level).width + 45
+    // XP score
+    ctx.fillText(score, x, row.y)
+
+    // next row y coordinate
+    const lineHeight =
+      ctx.measureText(score).actualBoundingBoxAscent +
+      ctx.measureText(score).actualBoundingBoxDescent
+    row.y += lineHeight + rowGap
+  })
+
   return new MessageAttachment(canvas.toBuffer(), "leaderboard.png")
 }
 
