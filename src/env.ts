c86527543f481e65bee7ceafdcb16a3a8eb2a660
anhnh12 <nguyenhoanganh0772@gmail.com>
2022-05-27 19:21:10 +0700

    feat: profile - add fraction XPs

--- src/env.ts
+++ src/env.ts
@@ -11,6 +11,9 @@ export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 export const API_SERVER_HOST =
   process.env.API_SERVER_HOST || "http://localhost:8200"
 
+export const PT_API_SERVER_HOST =
+  process.env.PT_API_SERVER_HOST || "https://backend.pod.so"
+
 export const LOG_CHANNEL_ID = process.env.LOG_CHANNEL_ID || "932579148608729118"
 export const DISCORD_DEFAULT_AVATAR = process.env.DISCORD_DEFAULT_AVATAR || ""
 export const WEBSITE_ENDPOINT = process.env.WEBSITE_ENDPOINT || ""
