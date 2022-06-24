e0c5d56cbd835976e1588375d9121d8fbf082cda
NMINHDAI <58351008+NMINHDAI@users.noreply.github.com>
2022-06-24 13:19:44 +0700

    feat: create renderSaleMessages (#121)

---
+++ src/types/sales.ts
@@ -0,0 +1,15 @@
+export type sales = {
+  user_level: string
+  marketplace: string
+  fromAddress: string
+  price: string
+  hodl: string
+  rank: number
+  transactionAddress: string
+  toAddress: string
+  bought: string
+  gain: string
+  sold: string
+  pnl: string
+  subPnl: string
+}
