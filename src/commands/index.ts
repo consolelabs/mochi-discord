a29776e141c3d95addaecdd661b0192fdfb10dea
NMINHDAI <ngominhdai92tn@gmail.com>
2022-06-26 11:46:52 +0700

    chore: update UI forsales

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -39,7 +39,6 @@ import nftrole from "./config/nftRole"
 import globalxp from "./config/globalxp"
 import eventxp from "./config/eventxp"
 import log from "./config/log"
-import testdemo from "./renderSalesMessage"
 import { Command, Category } from "types/common"
 import { getCommandArguments } from "utils/commands"
 import { hasAdministrator } from "utils/common"
@@ -51,7 +50,6 @@ export const originalCommands: Record<string, Command> = {
   help,
   // profile
   profile,
-  testdemo,
   // defi
   deposit,
   tip,
