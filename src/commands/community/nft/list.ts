572d72508e1abc1ca541e1b660afd24ec841ee30
tuanddd <vincenttuandao@gmail.com>
2022-06-30 12:54:11 +0700

    refactor: pagination

--- src/commands/community/nft/list.ts
+++ src/commands/community/nft/list.ts
@@ -6,36 +6,9 @@ import {
   composeEmbedMessage,
   getErrorEmbed,
   getPaginationRow,
+  listenForPaginateAction,
 } from "utils/discordEmbed"
 import Community from "adapters/community"
-import { MessageComponentTypes } from "discord.js/typings/enums"
-
-async function handlePagination(replyMsg: Message, originalMsg: Message) {
-  const operators: Record<string, number> = {
-    "+": 1,
-    "-": -1,
-  }
-  replyMsg
-    .createMessageComponentCollector({
-      componentType: MessageComponentTypes.BUTTON,
-      idle: 60000,
-    })
-    .on("collect", async (i) => {
-      await i.deferUpdate()
-      const [pageStr, opStr, totalPage] = i.customId.split("_").slice(1)
-      const page = +pageStr + operators[opStr]
-      const {
-        messageOptions: { embeds },
-      } = await composeNFTListEmbed(originalMsg, page)
-      await replyMsg.edit({
-        embeds,
-        components: getPaginationRow(page, +totalPage),
-      })
-    })
-    .on("end", () => {
-      replyMsg.edit({ components: [] })
-    })
-}
 
 async function composeNFTListEmbed(msg: Message, pageIdx: number) {
   const { data, page, size, total } = await Community.getNFTCollections({
@@ -103,7 +76,7 @@ const command: Command = {
   run: async function (msg: Message) {
     const msgOpts = await composeNFTListEmbed(msg, 0)
     const reply = await msg.reply(msgOpts.messageOptions)
-    await handlePagination(reply, msg)
+    listenForPaginateAction(reply, msg, composeNFTListEmbed)
     return {
       messageOptions: null,
     }
