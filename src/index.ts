d46ff10523cf65dddd81fc09e58cc20cb054996f
NMINHDAI <ngominhdai92tn@gmail.com>
2022-07-05 13:42:01 +0700

    feat: create slash cmt for sale track

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Discord from "discord.js"
 import events from "./events"
-import { DISCORD_TOKEN } from "./env"
+import { DISCORD_TOKEN, LOG_CHANNEL_ID } from "./env"
+import community from "adapters/community"
+import { capFirst } from "utils/common"
 
 const client = new Discord.Client({
   intents: [
@@ -12,6 +14,91 @@ const client = new Discord.Client({
     Discord.Intents.FLAGS.GUILD_INVITES,
   ],
   partials: ["MESSAGE", "REACTION", "CHANNEL"],
+})
+
+client.on("ready", () => {
+  const guild = client.guilds.cache.get(LOG_CHANNEL_ID)
+  let commands
+  if (guild) {
+    commands = guild.commands
+  } else {
+    commands = client.application?.commands
+  }
+
+  commands?.create({
+    name: "track",
+    description: "Set a tracker for an NFT.",
+    options: [
+      {
+        name: "type",
+        description: "type",
+        required: true,
+        choices: [
+          {
+            name: "sales",
+            value: "sales",
+          },
+        ],
+        type: "STRING",
+      },
+      {
+        name: "channel",
+        description: "the channel",
+        required: true,
+        type: "CHANNEL",
+      },
+      {
+        name: "collection-address",
+        description: "address",
+        required: true,
+        type: "STRING",
+      },
+      {
+        name: "chain",
+        description: "platform",
+        required: true,
+        type: "STRING",
+      },
+    ],
+  })
+})
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) {
+    return
+  }
+  const { commandName, options } = interaction
+
+  if (commandName === "track") {
+    const addr = options.getString("collection-address")
+    const platform = options.getString("chain")
+    const channelArg = options.getChannel("channel")
+    ;(async () => {
+      try {
+        await community.createSalesTracker(
+          addr,
+          platform,
+          LOG_CHANNEL_ID,
+          channelArg.toString()
+        )
+
+        await interaction.deferReply({
+          ephemeral: true,
+        })
+
+        await new Promise((resolve) => setTimeout(resolve, 5000))
+        await interaction.editReply({
+          content: `Successfully configure ${channelArg} as sales update channel. Tracked contract address ${addr} on platform ${capFirst(
+            platform
+          )}.`,
+        })
+      } catch (error) {
+        await interaction.reply(
+          "Something went wrong! Please try again or contact administrators"
+        )
+      }
+    })()
+  }
 })
 
 // discord client
