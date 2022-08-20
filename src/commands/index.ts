a34eddc68c24ab26455a01fbbbf959b0d08e60da
anhnh12 <nguyenhoanganh0772@gmail.com>
2022-08-20 20:21:00 +0700

    chore: update command error alert

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -224,7 +224,7 @@ export default async function handlePrefixedCommand(message: Message) {
     } else {
       logger.error(e as string)
       await message.reply({ embeds: [getErrorEmbed({ msg: message })] })
-      ChannelLogger.alert(message)
+      ChannelLogger.alert(message, error)
     }
     ChannelLogger.log(error)
   }
