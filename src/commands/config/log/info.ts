25a1b04b78db8445f60a039cd510e33c905c8e22
anhnh12 <nguyenhoanganh0772@gmail.com>
2022-08-23 18:25:20 +0700

    feat: view log channel cmd (#243)

---
+++ src/commands/config/log/info.ts
@@ -0,0 +1,43 @@
+import config from "adapters/config"
+import { Command } from "types/common"
+import { PREFIX } from "utils/constants"
+import { composeEmbedMessage } from "utils/discordEmbed"
+
+const command: Command = {
+  id: "log_info",
+  command: "info",
+  brief: "Show current logging channel info",
+  category: "Config",
+  onlyAdministrator: true,
+  run: async function (msg) {
+    const guild = await config.getGuild(msg.guildId)
+    if (!guild) {
+      throw new Error(`Guild ${msg.guildId} not found`)
+    }
+    if (!guild.log_channel) {
+      const embed = composeEmbedMessage(msg, {
+        author: [msg.guild.name, msg.guild.iconURL()],
+        description: `No logging channel configured for this guild.\nSet one with \`${PREFIX}log set <channel>.\``,
+      })
+      return { messageOptions: { embeds: [embed] } }
+    }
+
+    const embed = composeEmbedMessage(msg, {
+      author: [msg.guild.name, msg.guild.iconURL()],
+      description: `Current monitoring channel is <#${guild.log_channel}>.\nYou can update using \`${PREFIX}log set <channel>.\``,
+    })
+    return { messageOptions: { embeds: [embed] } }
+  },
+  getHelpMessage: async (msg) => ({
+    embeds: [
+      composeEmbedMessage(msg, {
+        usage: `${PREFIX}log info`,
+        examples: `${PREFIX}log info`,
+      }),
+    ],
+  }),
+  canRunWithoutAction: true,
+  colorType: "Server",
+}
+
+export default command
