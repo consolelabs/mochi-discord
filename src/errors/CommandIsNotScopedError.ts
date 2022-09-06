bb8484bf2743853e8fa7a1920cd068621fe2c028
anhnh12 <nguyenhoanganh0772@gmail.com>
2022-09-06 20:58:39 +0700

    feat: slash commands migration (#279)

--- src/errors/CommandIsNotScopedError.ts
+++ src/errors/CommandIsNotScopedError.ts
@@ -1,6 +1,7 @@
-import { Message, TextChannel } from "discord.js"
+import { CommandInteraction, Message, TextChannel } from "discord.js"
 import { BotBaseError } from "./BaseError"
 
+// TODO: remove after slash command migration done
 export class CommandIsNotScopedError extends BotBaseError {
   constructor({
     message,
@@ -18,6 +19,28 @@ export class CommandIsNotScopedError extends BotBaseError {
       guild: message.guild?.name,
       channel: channel.name,
       user: message.author.tag,
+      data: { category, command },
+    })
+  }
+}
+
+export class SlashCommandIsNotScopedError extends BotBaseError {
+  constructor({
+    interaction,
+    category,
+    command,
+  }: {
+    interaction: CommandInteraction
+    category: string
+    command: string
+  }) {
+    super()
+    this.name = "Slash command is not scoped"
+    const channel = interaction.channel as TextChannel
+    this.message = JSON.stringify({
+      guild: interaction.guild?.id,
+      channel: channel.id,
+      user: interaction.user.id,
       data: { category, command },
     })
   }
