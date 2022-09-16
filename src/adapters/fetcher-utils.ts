bc56423605c61f671e21b95a3c0c5123ce1d08ff
TuanD <vincenttuandao@gmail.com>
2022-09-16 15:04:14 +0700

    fix: convert snakecase array (#344)

--- src/adapters/fetcher-utils.ts
+++ src/adapters/fetcher-utils.ts
@@ -1,13 +1,23 @@
 import { snakeCase } from "change-case"
 
+function isObject(v: any): v is object {
+  return typeof v === "object" && v !== null && !Array.isArray(v)
+}
+
 export function convertToSnakeCase<T extends Record<string | number, any>>(
   obj: T
 ): T {
   const converted: Record<string | number, any> = {}
   for (const [key, value] of Object.entries(obj)) {
-    const isObject = typeof value === "object" && value !== null
-    if (isObject) {
+    if (isObject(value)) {
       converted[snakeCase(key)] = convertToSnakeCase(value)
+    } else if (Array.isArray(value)) {
+      converted[snakeCase(key)] = value.map((v) => {
+        if (isObject(v)) {
+          return convertToSnakeCase(v)
+        }
+        return v
+      })
     } else {
       converted[snakeCase(key)] = value
     }
