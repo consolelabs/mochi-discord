2a19c4c47dd2e3554ae64cbe764c7c84cb2021ab
TuanD <vincenttuandao@gmail.com>
2022-09-22 15:51:58 +0700

    feat: assign role after verify (#385)

--- src/errors/GuildIdNotFoundError.ts
+++ src/errors/GuildIdNotFoundError.ts
@@ -5,15 +5,22 @@ import { BotBaseError } from "./BaseError"
 export class GuildIdNotFoundError extends BotBaseError {
   private errorMsg: string
 
-  constructor({ message }: { message: Message | MessageComponentInteraction }) {
+  constructor({
+    message,
+  }: {
+    message?: Message | MessageComponentInteraction
+  }) {
     super(message)
     this.name = "Guild ID not found"
     this.errorMsg =
       "User invoked a command that was likely in a DM because guild id can not be found"
-    const channel = message.channel as TextChannel
-    const user = "author" in message ? message.author.tag : message.user.tag
+    const channel = message?.channel as TextChannel
+    const user =
+      message && "author" in message
+        ? message.author.tag
+        : message?.user.tag ?? "Unknown"
     this.message = JSON.stringify({
-      guild: message.guild ? message.guild.name : "",
+      guild: message?.guild ? message.guild.name : "DM",
       channel: channel?.name ?? "DM",
       user,
     })
