7b45702de9bd10b1da363d5cd9631fefd88c071a
catng <84314071+catngh@users.noreply.github.com>
2022-09-23 10:37:50 +0700

    feat: prune config (#388)

---
+++ src/commands/community/prune/remove.ts
@@ -0,0 +1,63 @@
+import config from "adapters/config"
+import { Guild, Message, User } from "discord.js"
+import { APIError, CommandError, GuildIdNotFoundError } from "errors"
+import { Command } from "types/common"
+import { getCommandArguments, parseDiscordToken } from "utils/commands"
+import { PREFIX } from "utils/constants"
+import { composeEmbedMessage, getSuccessEmbed } from "utils/discordEmbed"
+
+async function deleteWhitelist(roleId: string, guild: Guild, user: User) {
+  const res = await config.removeExcludedRole(roleId, guild.id)
+  if (!res.ok) {
+    throw new APIError({ curl: res.curl, description: res.log, guild, user })
+  }
+}
+
+const command: Command = {
+  id: "prune_remove",
+  command: "remove",
+  brief: "Remove a whitelisted role",
+  category: "Community",
+  run: async (msg: Message) => {
+    if (!msg.guild) {
+      throw new GuildIdNotFoundError({ message: msg })
+    }
+
+    const args = getCommandArguments(msg)
+    const { isRole, id } = parseDiscordToken(args[2])
+    if (!isRole) {
+      throw new CommandError({
+        message: msg,
+        description: "The argument is not a role",
+      })
+    }
+
+    await deleteWhitelist(id, msg.guild, msg.author)
+
+    return {
+      messageOptions: {
+        embeds: [
+          getSuccessEmbed({
+            msg,
+            title: "Successfully removed!",
+            description: `<@&${id}> is no longer whitelisted.`,
+          }),
+        ],
+      },
+    }
+  },
+  getHelpMessage: async (msg) => ({
+    embeds: [
+      composeEmbedMessage(msg, {
+        usage: `${PREFIX}prune remove <role>`,
+        examples: `${PREFIX}prune remove @Mochi`,
+        includeCommandsList: true,
+      }),
+    ],
+  }),
+  colorType: "Server",
+  onlyAdministrator: true,
+  minArguments: 3,
+}
+
+export default command
---
+++ src/commands/community/prune/whitelist.ts
@@ -0,0 +1,110 @@
+import config from "adapters/config"
+import { Guild, Message, Role, User } from "discord.js"
+import {
+  APIError,
+  BotBaseError,
+  CommandError,
+  GuildIdNotFoundError,
+} from "errors"
+import { Command } from "types/common"
+import { getCommandArguments, parseDiscordToken } from "utils/commands"
+import { PREFIX } from "utils/constants"
+import { composeEmbedMessage, getSuccessEmbed } from "utils/discordEmbed"
+
+async function createWhitelist(roleId: string, guild: Guild, user: User) {
+  const res = await config.createExcludedRole(roleId, guild.id)
+  if (!res.ok) {
+    throw new APIError({ curl: res.curl, description: res.log, guild, user })
+  }
+}
+
+export async function getExcludedRoles(guild: Guild): Promise<Role[]> {
+  let roleIds: string[] = []
+  const excludedRoles: Role[] = []
+  const res = await config.getExcludedRole({ guild_id: guild.id })
+  if (!res.ok) {
+    throw new BotBaseError()
+  }
+  if (res.data) {
+    roleIds = res.data.roles ?? []
+  }
+
+  roleIds.forEach(async (id) => {
+    const role = await guild.roles.fetch(id)
+    if (role) {
+      excludedRoles.push(role)
+    }
+  })
+
+  return excludedRoles
+}
+
+export async function whitelistRolesEmbed(roles: Role[]) {
+  let roleStr = ""
+  roles.forEach((role) => {
+    roleStr += role.name + ", "
+  })
+  return composeEmbedMessage(null, {
+    title: "Prune Whitelisted Roles",
+    description: `Roles that are excluded when performing \`$prune\`: ${roleStr}`,
+  })
+}
+
+const command: Command = {
+  id: "prune_whitelist",
+  command: "whitelist",
+  brief: "Whitelist a role from being pruned",
+  category: "Community",
+  run: async (msg: Message) => {
+    if (!msg.guild) {
+      throw new GuildIdNotFoundError({ message: msg })
+    }
+
+    const args = getCommandArguments(msg)
+    //$prune whitelist
+    if (args.length == 2) {
+      const roles = await getExcludedRoles(msg.guild)
+      const embed = await whitelistRolesEmbed(roles)
+      return {
+        messageOptions: {
+          embeds: [embed],
+        },
+      }
+    }
+
+    const { isRole, id } = parseDiscordToken(args[2])
+    if (!isRole) {
+      throw new CommandError({
+        message: msg,
+        description: "The argument is not a role",
+      })
+    }
+
+    await createWhitelist(id, msg.guild, msg.author)
+
+    return {
+      messageOptions: {
+        embeds: [
+          getSuccessEmbed({
+            msg,
+            title: "Successfully set!",
+            description: `<@&${id}> successfully whitelisted`,
+          }),
+        ],
+      },
+    }
+  },
+  getHelpMessage: async (msg) => ({
+    embeds: [
+      composeEmbedMessage(msg, {
+        usage: `${PREFIX}prune whitelist <role>\n${PREFIX}prune whitelist`,
+        examples: `${PREFIX}prune whitelist\n${PREFIX}prune whitelist @Mochi`,
+        includeCommandsList: true,
+      }),
+    ],
+  }),
+  colorType: "Server",
+  onlyAdministrator: true,
+}
+
+export default command
