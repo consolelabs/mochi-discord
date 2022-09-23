1e159ce8a6880a317efbf67d9eff63d80d1d88b8
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2022-09-23 10:42:42 +0700

    refactor: starboard flow (#387)

--- src/commands/config/starboard/remove.ts
+++ src/commands/config/starboard/remove.ts
@@ -3,7 +3,8 @@ import { PREFIX } from "utils/constants"
 import { composeEmbedMessage, getErrorEmbed } from "utils/discordEmbed"
 import { Message } from "discord.js"
 import config from "adapters/config"
-import { getCommandArguments } from "utils/commands"
+import { getCommandArguments, parseDiscordToken } from "utils/commands"
+import { GuildIdNotFoundError } from "errors"
 
 const command: Command = {
   id: "starboard_remove",
@@ -14,17 +15,13 @@ const command: Command = {
   run: async (msg: Message) => {
     const args = getCommandArguments(msg)
     // Validate input reaction emoji
-    let reaction = args[2]
+    const reaction = args[2]
     let isValidEmoji = false
-    if (reaction.startsWith("<:") && reaction.endsWith(">")) {
-      reaction = reaction.toLowerCase()
-    }
-    const emojiSplit = reaction.split(":")
-    if (emojiSplit.length === 1) {
-      isValidEmoji = true
-    }
-    msg.guild.emojis.cache.forEach((e) => {
-      if (emojiSplit.includes(e.name.toLowerCase())) {
+
+    const { id } = parseDiscordToken(reaction)
+
+    msg.guild?.emojis.cache.forEach((e) => {
+      if (id == e.id) {
         isValidEmoji = true
       }
     })
@@ -39,6 +36,10 @@ const command: Command = {
           ],
         },
       }
+    }
+
+    if (!msg.guild) {
+      throw new GuildIdNotFoundError({ message: msg })
     }
 
     const requestData: RepostReactionRequest = {
--- src/commands/config/starboard/set.ts
+++ src/commands/config/starboard/set.ts
@@ -4,11 +4,12 @@ import { composeEmbedMessage, getErrorEmbed } from "utils/discordEmbed"
 import { Message } from "discord.js"
 import config from "adapters/config"
 import { getCommandArguments } from "utils/commands"
+import { GuildIdNotFoundError } from "errors"
 
 const command: Command = {
-  id: "starboard_add",
-  command: "add",
-  brief: "Add or update a starboard configuration",
+  id: "starboard_set",
+  command: "set",
+  brief: "Set or update a starboard configuration",
   category: "Config",
   onlyAdministrator: true,
   run: async (msg: Message) => {
@@ -33,8 +34,8 @@ const command: Command = {
     if (emojiSplit.length === 1) {
       isValidEmoji = true
     }
-    msg.guild.emojis.cache.forEach((e) => {
-      if (emojiSplit.includes(e.name.toLowerCase())) {
+    msg.guild?.emojis.cache.forEach((e) => {
+      if (emojiSplit.includes(e.name!.toLowerCase())) {
         isValidEmoji = true
       }
     })
@@ -53,7 +54,7 @@ const command: Command = {
 
     // Validate repost_channel_id args
     const channelId = args[4].replace(/\D/g, "")
-    const channel = await msg.guild.channels.fetch(channelId)
+    const channel = await msg.guild?.channels.fetch(channelId)
     if (!channel || !channelId) {
       return {
         messageOptions: {
@@ -66,6 +67,10 @@ const command: Command = {
           ],
         },
       }
+    }
+
+    if (!msg.guild) {
+      throw new GuildIdNotFoundError({ message: msg })
     }
 
     const requestData: RepostReactionRequest = {
@@ -93,8 +98,8 @@ const command: Command = {
     return {
       embeds: [
         composeEmbedMessage(msg, {
-          usage: `${PREFIX}sb add <quantity> <emoji> <channel>`,
-          examples: `${PREFIX}sb add 3 ⭐ #starboard`,
+          usage: `${PREFIX}sb set <quantity> <emoji> <channel>`,
+          examples: `${PREFIX}sb set 3 ⭐ #starboard`,
         }),
       ],
     }
