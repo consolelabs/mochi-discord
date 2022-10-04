db196a5b0400b2a3d39e9e7dbf99e8799366e322
hoangharry <hoang.ho.it99@gmail.com>
2022-10-04 20:22:00 +0700

    fix: remove defer selection

--- src/commands/defi/ticker_slash.ts
+++ src/commands/defi/ticker_slash.ts
@@ -125,7 +125,6 @@ const getChangePercentage = (change: number) => {
 
 const handler: CommandChoiceHandler = async (msgOrInteraction) => {
   const interaction = msgOrInteraction as SelectMenuInteraction
-  await interaction.deferUpdate()
   const { message } = <{ message: Message }>interaction
   const input = interaction.values[0]
   const [coinId, days] = input.split("_")
@@ -177,8 +176,6 @@ const tickerSelectionHandler: CommandChoiceHandler = async (
   msgOrInteraction
 ) => {
   const interaction = msgOrInteraction as SelectMenuInteraction
-  await interaction.deferUpdate()
-  // const { message } = <{ message: Message }>interaction
   const value = interaction.values[0]
   const [coinId, coinSymbol, coinName, authorId] = value.split("_")
   return await composeTickerResponse({
