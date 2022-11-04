e72d4781afb5994f1e4068f126daa37bc0e4365f
catng <hoangcat622001@gmail.com>
2022-11-05 01:22:57 +0700

    chore: nft slash

--- src/errors/DirectMessageNotAllowedError.ts
+++ src/errors/DirectMessageNotAllowedError.ts
@@ -1,4 +1,4 @@
-import { Message, TextChannel } from "discord.js"
+import { CommandInteraction, Message, TextChannel } from "discord.js"
 import { getErrorEmbed } from "utils/discordEmbed"
 import { BotBaseError } from "./BaseError"
 
@@ -25,6 +25,35 @@ export class DirectMessageNotAllowedError extends BotBaseError {
           msg: this.discordMessage,
           title: "DM not enabled",
           description: `<@!${this.discordMessage.author.id}>, could not send you a direct message.\nPlease make sure that you have enabled \`Allow direct messages from server members\` in User Settings.`,
+          image: "https://i.imgur.com/ctGqgyf.png",
+        }),
+      ],
+    })
+  }
+}
+
+export class DirectMessageNotAllowedSlashError extends BotBaseError {
+  private interaction: CommandInteraction
+
+  constructor({ interaction }: { interaction: CommandInteraction }) {
+    super()
+    this.interaction = interaction
+    this.name = "DM not enabled"
+    const channel = interaction.channel as TextChannel
+    this.message = JSON.stringify({
+      guild: interaction.guild?.name,
+      channel: channel.name,
+      user: interaction.user.tag,
+    })
+  }
+
+  handle() {
+    super.handle()
+    this.interaction.channel?.send({
+      embeds: [
+        getErrorEmbed({
+          title: "DM not enabled",
+          description: `<@!${this.interaction.user.id}>, could not send you a direct message.\nPlease make sure that you have enabled \`Allow direct messages from server members\` in User Settings.`,
           image: "https://i.imgur.com/ctGqgyf.png",
         }),
       ],
