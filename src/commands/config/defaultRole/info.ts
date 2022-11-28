c19f1c4d9a3e4364a6a5ecb045741d53a1a8e706
TuanD <vincenttuandao@gmail.com>
2022-11-28 16:19:28 +0700

    refactor: default role (#624)

--- src/commands/config/defaultRole/info.ts
+++ src/commands/config/defaultRole/info.ts
@@ -1,10 +1,53 @@
 import { Command } from "types/common"
 import { DEFAULT_ROLE_GITBOOK, PREFIX } from "utils/constants"
-import { composeEmbedMessage, getErrorEmbed } from "utils/discordEmbed"
-import { Message } from "discord.js"
+import { composeEmbedMessage, composeEmbedMessage2 } from "utils/discordEmbed"
 import config from "adapters/config"
 import { getCommandArguments } from "utils/commands"
 import { emojis, getEmojiURL } from "utils/common"
+import { APIError, GuildIdNotFoundError, OriginalMessage } from "errors"
+import { CommandInteraction, Message } from "discord.js"
+
+export async function handle(msg: OriginalMessage, statusText = "") {
+  if (!msg.guildId) {
+    throw new GuildIdNotFoundError({})
+  }
+
+  let description = `No default role found! To set, run \`\`\`${PREFIX}dr set @<role>\`\`\``
+
+  const res = await config.getCurrentDefaultRole(msg.guildId)
+  if (res.ok) {
+    if (res.data.role_id) {
+      description = `When people first join your server, their base role will be <@&${res.data.role_id}>`
+    } else {
+      description = `No default role found! To set, run \`\`\`${PREFIX}dr set @<role>\`\`\``
+    }
+  } else {
+    throw new APIError({
+      curl: res.curl,
+      error: res.error,
+      description: res.log,
+      message: msg,
+    })
+  }
+
+  const embed =
+    msg instanceof Message
+      ? composeEmbedMessage(msg, {
+          author: ["Default role", getEmojiURL(emojis.NEKO1)],
+          description,
+        })
+      : composeEmbedMessage2(msg as CommandInteraction, {
+          author: ["Default role", getEmojiURL(emojis.NEKO1)],
+          description,
+        })
+
+  return {
+    messageOptions: {
+      ...(statusText ? { content: `> ${statusText}` } : {}),
+      embeds: [embed],
+    },
+  }
+}
 
 const command: Command = {
   id: "defaultrole_info",
@@ -12,20 +55,7 @@ const command: Command = {
   brief: "Show current default role for newcomers",
   category: "Config",
   onlyAdministrator: true,
-  run: async (msg: Message) => {
-    if (!msg.guildId || !msg.guild) {
-      return {
-        messageOptions: {
-          embeds: [
-            getErrorEmbed({
-              msg,
-              description: "This command must be run in a Guild",
-            }),
-          ],
-        },
-      }
-    }
-    let description = `No default role found! To set, run \`\`\`${PREFIX}dr set @<role>\`\`\``
+  run: async (msg) => {
     const args = getCommandArguments(msg)
 
     if (args.length !== 2) {
@@ -41,31 +71,7 @@ const command: Command = {
       }
     }
 
-    const res = await config.getCurrentDefaultRole(msg.guildId)
-    if (res.ok) {
-      if (res.data.role_id) {
-        description = `When people first join your server, their base role will be <@&${res.data.role_id}>`
-      } else {
-        description = `No default role found! To set, run \`\`\`${PREFIX}dr set @<role>\`\`\``
-      }
-    } else {
-      return {
-        messageOptions: {
-          embeds: [getErrorEmbed({ msg, description: res.error })],
-        },
-      }
-    }
-
-    return {
-      messageOptions: {
-        embeds: [
-          composeEmbedMessage(msg, {
-            author: ["Default role", getEmojiURL(emojis.NEKO1)],
-            description,
-          }),
-        ],
-      },
-    }
+    return handle(msg)
   },
   getHelpMessage: async (msg) => {
     return {
--- src/commands/config/defaultRole_slash/remove.ts
+++ src/commands/config/defaultRole_slash/remove.ts
@@ -1,13 +1,11 @@
 import config from "adapters/config"
-import {
-  composeEmbedMessage2,
-  getErrorEmbed,
-  getSuccessEmbed,
-} from "utils/discordEmbed"
+import { composeEmbedMessage2 } from "utils/discordEmbed"
 import { SlashCommandSubcommandBuilder } from "@discordjs/builders"
 import { CommandInteraction } from "discord.js"
 import { SlashCommand } from "types/common"
 import { SLASH_PREFIX } from "utils/constants"
+import { APIError, GuildIdNotFoundError } from "errors"
+import { handle } from "../defaultRole/info"
 
 const command: SlashCommand = {
   name: "remove",
@@ -19,34 +17,20 @@ const command: SlashCommand = {
   },
   run: async function (interaction: CommandInteraction) {
     if (!interaction.guildId || !interaction.guild) {
-      return {
-        messageOptions: {
-          embeds: [
-            getErrorEmbed({
-              description: "This command must be run in a Guild",
-            }),
-          ],
-        },
-      }
-    }
-    let description = ""
-    const res = await config.removeDefaultRoleConfig(interaction.guildId)
-    if (res.ok) {
-      description =
-        "Existing users' role will not be affected\nHowever please **NOTE** that from now on new users joining your server won't have a default role anymore.\nTo set a new one, run `/defaultrole set @<role_name>`"
-    } else {
-      return {
-        messageOptions: {
-          embeds: [getErrorEmbed({ description: res.error })],
-        },
-      }
+      throw new GuildIdNotFoundError({})
     }
 
-    return {
-      messageOptions: {
-        embeds: [getSuccessEmbed({ title: "Role removed", description })],
-      },
+    const res = await config.removeDefaultRoleConfig(interaction.guildId)
+    if (!res.ok) {
+      throw new APIError({
+        message: interaction,
+        error: res.error,
+        description: res.log,
+        curl: res.curl,
+      })
     }
+
+    return handle(interaction, "Default role removed")
   },
   help: async (interaction: CommandInteraction) => ({
     embeds: [
