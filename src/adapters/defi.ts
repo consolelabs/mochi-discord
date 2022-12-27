f9b38379d0037fa0804e126d4f2ff26fd1127d1e
Cat Nguyen <hoangcat622001@gmail.com>
2022-12-27 17:27:58 +0700

    chore: test parser

--- src/adapters/defi.ts
+++ src/adapters/defi.ts
@@ -720,6 +720,7 @@ class Defi extends Fetcher {
         .replaceAll('"', "")
         .replaceAll("”", "")
         .replaceAll("“", "")
+        .replaceAll("'", "")
         .trim()
     }
     return {
