63a8b6f7b1a98e65bf6bbf28cfee5b1e900f7dfd
phucledien <phucledien@gmail.com>
2023-01-18 22:06:47 +0700

    feat: remove token role

--- src/commands/token-role/remove/slash.ts
+++ src/commands/token-role/remove/slash.ts
@@ -1,5 +1,14 @@
 import { SlashCommandSubcommandBuilder } from "@discordjs/builders"
+import config from "adapters/config"
+import { list } from "../processor"
+import { CommandInteraction, MessageSelectOptionData } from "discord.js"
+import { APIError, GuildIdNotFoundError, InternalError } from "errors"
 import { SlashCommand } from "types/common"
+import { composeDiscordExitButton } from "ui/discord/button"
+import { composeEmbedMessage2 } from "ui/discord/embed"
+import { composeDiscordSelectionRow } from "ui/discord/select-menu"
+import { SLASH_PREFIX, TOKEN_ROLE_GITBOOK } from "utils/constants"
+import { handler } from "./processor"
 
 const command: SlashCommand = {
   name: "remove",
@@ -7,6 +16,71 @@ const command: SlashCommand = {
   prepare: () => {
     return new SlashCommandSubcommandBuilder()
       .setName("remove")
-      .setDescription("Remove a token-role configuration")
+      .setDescription("Remove a token role setup")
   },
+  run: async (interaction: CommandInteraction) => {
+    if (!interaction.guildId || !interaction.guild) {
+      throw new GuildIdNotFoundError({ message: interaction })
+    }
+    const { ok, error, data, log, curl } = await config.getConfigTokenRoleList(
+      interaction.guildId
+    )
+    if (!ok) {
+      throw new APIError({
+        message: interaction,
+        description: log,
+        curl,
+        error,
+      })
+    }
+
+    const options: MessageSelectOptionData[] = []
+    for (const config of data) {
+      if (!config.role_id) {
+        throw new InternalError({
+          message: interaction,
+          description: "invalid role id",
+        })
+      }
+      const role = await interaction.guild?.roles.fetch(config.role_id)
+      options.push({
+        label: role?.name ?? "NA",
+        value: `${config.id ?? ""}|${role?.name ?? ""}`,
+      })
+    }
+
+    const embed = composeEmbedMessage2(interaction, {
+      title: "Select an option",
+      description: list({ data }).description,
+    })
+
+    return {
+      messageOptions: {
+        embeds: [embed],
+        components: [
+          composeDiscordSelectionRow({
+            customId: "tokenrole_remove",
+            placeholder: "Select a tokenrole",
+            options,
+          }),
+          composeDiscordExitButton(interaction.user.id),
+        ],
+      },
+      interactionOptions: {
+        handler,
+      },
+    }
+  },
+  help: async (interaction: CommandInteraction) => ({
+    embeds: [
+      composeEmbedMessage2(interaction, {
+        usage: `${SLASH_PREFIX}tokenrole remove`,
+        examples: `${SLASH_PREFIX}tokenrole remove`,
+        document: `${TOKEN_ROLE_GITBOOK}&action=remove`,
+      }),
+    ],
+  }),
+  colorType: "Server",
 }
+
+export default command
