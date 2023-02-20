75196f98175143ad866a82bfaf38946393fbea4c
Minh Hoang Ho <49946656+hoangharry@users.noreply.github.com>
2023-02-20 17:50:45 +0700

    feat: alert cmd (#800)

---
+++ src/commands/alert/list/processor.ts
@@ -0,0 +1,52 @@
+import defi from "adapters/defi"
+import { CommandInteraction, Message, MessageEmbed } from "discord.js"
+import { APIError } from "errors"
+import { composeEmbedMessage } from "ui/discord/embed"
+import { getEmoji, paginate } from "utils/common"
+
+export async function handleAlertList({
+  msg,
+  interaction,
+}: {
+  msg?: Message
+  interaction?: CommandInteraction
+}) {
+  let userId: string
+  if (msg) {
+    userId = msg.author.id
+  } else if (interaction) {
+    userId = interaction.user.id
+  } else {
+    return null
+  }
+
+  const { ok, data, log, curl } = await defi.getAlertList(userId)
+  if (!ok) {
+    throw new APIError({ curl, description: log })
+  }
+
+  if (!data) {
+    return []
+  }
+
+  let pages = paginate(data as [], 10)
+  pages = pages.map((arr: any, idx: number): MessageEmbed => {
+    let description = ""
+    arr.forEach(
+      (item: any) =>
+        (description += `**${item.symbol}** - ${item.frequency.replaceAll(
+          "_",
+          " "
+        )}\n${getEmoji("reply")} When ${item.alert_type.replaceAll(
+          "_",
+          " "
+        )} **${item.price}**\n`)
+    )
+    return composeEmbedMessage(null, {
+      title: `${getEmoji("increasing")} Alert list`,
+      description,
+      footer: [`Page ${idx + 1} / ${pages.length}`],
+    })
+  })
+  return pages
+}
