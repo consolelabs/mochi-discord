2220ae53beec13b39869d4e8c624faa7439cfbbc
trkhoi <ngotrongkhoi112@gmail.com>
2023-03-16 14:42:32 +0700

    feat: gas tracker one chain

--- src/commands/gas/index/slash.ts
+++ src/commands/gas/index/slash.ts
@@ -1,6 +1,11 @@
-import { render } from "./processor"
+import { render, renderOne } from "./processor"
+import { CommandInteraction } from "discord.js"
 
-const run = async () => {
+const run = async (i: CommandInteraction) => {
+  const chain = i.options.getString("chain")
+  if (chain) {
+    return await renderOne(chain)
+  }
   return await render()
 }
 export default run
