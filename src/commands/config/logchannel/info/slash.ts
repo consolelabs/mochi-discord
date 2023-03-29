da977b9b3b039a4f78ed61778dc62c1e11250978
tuanddd <vincenttuandao@gmail.com>
2023-03-29 18:40:21 +0700

    feat: config logchannel command

---
+++ src/commands/config/logchannel/info/slash.ts
@@ -0,0 +1,62 @@
+import { SlashCommandSubcommandBuilder } from "@discordjs/builders"
+import { embedsColors, SlashCommand } from "types/common"
+import { composeEmbedMessage } from "ui/discord/embed"
+import { getEmoji } from "utils/common"
+import { HOMEPAGE_URL } from "utils/constants"
+import get from "./processor"
+
+const slashCmd: SlashCommand = {
+  name: "info",
+  category: "Config",
+  prepare: () => {
+    const data = new SlashCommandSubcommandBuilder()
+      .setName("info")
+      .setDescription("View logchannel config")
+
+    return data
+  },
+  run: async function (i) {
+    const config = await get(i)
+    const isEmpty = Object.values(config).flat().length === 0
+    const list = Object.entries(config)
+      ?.map(
+        (c) =>
+          `\`${c[0]}>\`\n${c[1]
+            ?.map(
+              (channelId) =>
+                `${getEmoji("blank")}${getEmoji("reply")}<#${channelId}>`
+            )
+            .join("\n")}`
+      )
+      .join("\n\n")
+
+    return {
+      messageOptions: {
+        embeds: [
+          composeEmbedMessage(null, {
+            color: embedsColors.Profile,
+            author: [
+              "Log Channel Info",
+              "https://cdn.discordapp.com/emojis/1090477901725577287.webp?size=240&quality=lossless",
+            ],
+            thumbnail:
+              "https://cdn.discordapp.com/attachments/1052079279619457095/1090587614450565140/logchannel_info.png",
+            description: `Log channel is a place to keep all records of every user’s activity and interaction with Mochi bot. That way, we can better monitor and support guild members' activities from withdrawal, airdrop, role assignment, and many more!\n\n${
+              isEmpty ? "`Not Set`" : list
+            }\n\n${getEmoji(
+              "pointingright"
+            )} Setup or change config by running \`/config logchannel set\`\n${getEmoji(
+              "pointingright"
+            )} Use \`/config logchannel info\` to see more details\n[Read instructions](${HOMEPAGE_URL}) for a complete setup guide`,
+          }),
+        ],
+      },
+    }
+  },
+  help: async () => ({
+    embeds: [composeEmbedMessage(null, { includeCommandsList: true })],
+  }),
+  colorType: "Server",
+}
+
+export default slashCmd
