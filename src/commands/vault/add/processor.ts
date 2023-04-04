efdf52148d1771e29a12a8d149f865d8fd5022da
trkhoi <ngotrongkhoi112@gmail.com>
2023-04-04 15:48:55 +0700

    feat: flow add treasurer

--- src/commands/vault/add/processor.ts
+++ src/commands/vault/add/processor.ts
@@ -1,10 +1,19 @@
 import config from "adapters/config"
-import { CommandInteraction } from "discord.js"
+import {
+  CommandInteraction,
+  MessageActionRow,
+  MessageButton,
+  ButtonInteraction,
+} from "discord.js"
 import { GuildIdNotFoundError } from "errors"
 import { MessageEmbed } from "discord.js"
 import { APIError } from "errors"
 import { getEmoji, msgColors } from "utils/common"
-import { getErrorEmbed } from "ui/discord/embed"
+import {
+  getErrorEmbed,
+  composeEmbedMessage,
+  getSuccessEmbed,
+} from "ui/discord/embed"
 
 export async function runAddTreasurer({
   i,
@@ -42,6 +51,8 @@ export async function runAddTreasurer({
     user_discord_id: user.id,
     vault_name: vaultName,
     message: i.options.getString("message") ?? "",
+    type: "add",
+    requester: i.user.id,
   })
 
   if (statusAddTreasurerReq !== 200 && statusAddTreasurerReq !== 400) {
@@ -63,58 +74,135 @@ export async function runAddTreasurer({
     }
   }
 
-  // TODO: currently add treasurer directly after create request, in the future:
-  // - create req -> send DM to user with approve/ reject button
-  // - api add treasurer should be add to interaction of buttion approve / reject
-  //   const embed = new MessageEmbed()
-  //     .setTitle(
-  //       `${getEmoji("PROPOSAL")} Request #${data.id} successfully created`
-  //     )
-  //     .setDescription(
-  //       `You want to add <@${
-  //         user.id
-  //       }> to **${vaultName} vault**\nMessage ${getEmoji("MESSAGE2")}\n\n\`${
-  //         data.message
-  //       }\`\n\nWe'll notify you once all treasurers have accepted the request.`
-  //     )
-  //     .setColor(msgColors.GREEN)
-  //     .setFooter({ text: "Type /feedback to report • Mochi Bot" })
-  //     .setTimestamp(Date.now())
+  // send DM to submitter
+  dataAddTreasurerReq?.treasurer.forEach((treasurer: any) => {
+    const actionRow = new MessageActionRow().addComponents(
+      new MessageButton({
+        customId: `treasurerAdd-approved-${dataAddTreasurerReq?.request.id}-${dataAddTreasurerReq?.request.vault_id}-${dataAddTreasurerReq?.request.requester}-${dataAddTreasurerReq?.request.user_discord_id}-${i.channelId}`,
+        style: "SUCCESS",
+        label: "Approve",
+      }),
+      new MessageButton({
+        customId: `treasurerAdd-rejected-${dataAddTreasurerReq?.request.id}-${dataAddTreasurerReq?.request.vault_id}-${dataAddTreasurerReq?.request.requester}-${dataAddTreasurerReq?.request.user_discord_id}-${i.channelId}}`,
+        style: "DANGER",
+        label: "Reject",
+      })
+    )
 
-  const {
-    status: statusAddTreasurer,
-    curl: curlAddTreasurer,
-    log: logAddTreasurer,
-    originalError: originalErrorAddTreasurer,
-  } = await config.addTreasurerToVault({
-    request_id: dataAddTreasurerReq?.id,
+    i.guild?.members.fetch(treasurer.user_discord_id).then((treasurer) => {
+      treasurer.send({
+        embeds: [
+          composeEmbedMessage(null, {
+            title: `${getEmoji("BELL")} Mochi notifications`,
+            description: `**Approval Request #${
+              dataAddTreasurerReq?.request.id
+            }**\n<@${i.user.id}> has submitted a request\n${getEmoji(
+              "TREASURER"
+            )} Add <@${user.id}> to **${vaultName}**\nMessage ${getEmoji(
+              "MESSAGE2"
+            )}\n \`\`\`${dataAddTreasurerReq?.request.message}\`\`\``,
+            color: msgColors.MOCHI,
+            thumbnail:
+              "https://cdn.discordapp.com/attachments/1090195482506174474/1092703907911847976/image.png",
+          }),
+        ],
+        components: [actionRow],
+      })
+    })
   })
 
-  if (statusAddTreasurer !== 200 && statusAddTreasurer !== 400) {
+  // send DM to treasurer in vault
+
+  const embed = new MessageEmbed()
+    .setTitle(
+      `${getEmoji("PROPOSAL")} Request #${
+        dataAddTreasurerReq?.request.id
+      } successfully created`
+    )
+    .setDescription(
+      `You want to add <@${
+        user.id
+      }> to **${vaultName} vault**\n\nMessage ${getEmoji("MESSAGE2")}\n\`\`\`${
+        dataAddTreasurerReq?.request.message
+      }\`\`\`\nWe'll notify you once all treasurers have accepted the request.`
+    )
+    .setColor(msgColors.MOCHI)
+    .setFooter({ text: "Type /feedback to report • Mochi Bot" })
+    .setTimestamp(Date.now())
+    .setThumbnail(
+      "https://cdn.discordapp.com/attachments/1090195482506174474/1092703907911847976/image.png"
+    )
+
+  return { messageOptions: { embeds: [embed] } }
+}
+
+export async function handleTreasurerAdd(i: ButtonInteraction) {
+  await i.deferUpdate()
+  const args = i.customId.split("-")
+  const choice = args[1]
+  const requestId = args[2]
+  const vaultId = args[3]
+  // const guildId = args[4]
+  const submitter = args[4]
+  const user = args[5]
+  const channelId = args[6]
+
+  const {
+    data: dataAddTreasurer,
+    status,
+    curl,
+    log,
+    originalError,
+  } = await config.createTreasurerSubmissions({
+    vault_id: Number(vaultId),
+    request_id: Number(requestId),
+    submitter: submitter,
+    choice: choice,
+  })
+
+  if ((status !== 200 && status !== 400) || !dataAddTreasurer) {
     throw new APIError({
-      curl: curlAddTreasurer,
-      description: logAddTreasurer,
+      curl: curl,
+      description: log,
     })
   }
 
-  if (statusAddTreasurer === 400) {
+  if (status === 400) {
     return {
       messageOptions: {
         embeds: [
           getErrorEmbed({
-            description: originalErrorAddTreasurer,
+            description: originalError,
           }),
         ],
       },
     }
   }
 
-  const embed = new MessageEmbed()
-    .setTitle(`${getEmoji("APPROVE_VAULT")} Treasurer was successfullly added`)
-    .setDescription(`<@${user.id}> has been added to **${vaultName} vault**`)
-    .setColor(msgColors.GREEN)
-    .setFooter({ text: "Type /feedback to report • Mochi Bot" })
-    .setTimestamp(Date.now())
+  if (dataAddTreasurer.vote_result.is_approved === true) {
+    // add treasurer to vault
+    const { data, status, curl, log } = await config.addTreasurerToVault({
+      guild_id: dataAddTreasurer.submission.guild_id,
+      user_discord_id: user,
+      vault_id: Number(vaultId),
+      channel_id: channelId,
+    })
+    if ((status !== 200 && status !== 400) || !data) {
+      throw new APIError({
+        curl: curl,
+        description: log,
+      })
+    }
+  }
 
-  return { messageOptions: { embeds: [embed] } }
+  await i.editReply({
+    embeds: [
+      getSuccessEmbed({
+        title: "Successfully voted",
+        description: `You have updated your vote successfully. Thank you for your vote ${getEmoji(
+          "HEART"
+        )}`,
+      }),
+    ],
+  })
 }
