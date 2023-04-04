3351c56ca635dc32887ed41c80d98297677386f4
tuanddd <vincenttuandao@gmail.com>
2023-04-05 02:11:38 +0700

    feat: new default currency

---
+++ src/commands/config/currency/index/processor.ts
@@ -0,0 +1,52 @@
+import config from "adapters/config"
+import { CommandInteraction } from "discord.js"
+import { APIError, GuildIdNotFoundError } from "errors"
+
+export async function set(i: CommandInteraction, symbol: string) {
+  if (!i.guildId) {
+    throw new GuildIdNotFoundError({ message: i })
+  }
+
+  const { ok: getOk, data: currentData } = await config.getDefaultCurrency(
+    i.guildId
+  )
+
+  let was
+  if (getOk) {
+    was = {
+      symbol: currentData.tip_bot_token?.token_symbol,
+      name: currentData.tip_bot_token?.token_name,
+    }
+  }
+
+  const {
+    ok: setOk,
+    curl: setCurl,
+    log: setLog,
+  } = await config.setDefaultCurrency({ symbol, guild_id: i.guildId })
+
+  if (!setOk) {
+    throw new APIError({
+      msgOrInteraction: i,
+      curl: setCurl,
+      description: setLog,
+    })
+  }
+
+  const { ok: newOk, data: newData } = await config.getDefaultCurrency(
+    i.guildId
+  )
+
+  let now
+  if (newOk) {
+    now = {
+      symbol: newData.tip_bot_token?.token_symbol,
+      name: newData.tip_bot_token?.token_name,
+    }
+  }
+
+  return {
+    was,
+    now,
+  }
+}
