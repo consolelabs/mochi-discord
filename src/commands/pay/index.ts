ba7ac6f7596243f40d0dfc59076a69f91266d6d3
tuanddd <vincenttuandao@gmail.com>
2023-04-05 11:38:40 +0700

    feat: redirect pay with recipient command to tip command

--- src/commands/pay/index.ts
+++ src/commands/pay/index.ts
@@ -3,6 +3,7 @@ import { composeEmbedMessage } from "ui/discord/embed"
 import { PREFIX } from "utils/constants"
 import link from "./link/text"
 import me from "./me/text"
+import tip from "../new-tip/index/text"
 
 const actions: Record<string, Command> = {
   link,
@@ -14,7 +15,7 @@ const textCmd: Command = {
   command: "pay",
   brief: "Finish all due payments right in Discord",
   category: "Defi",
-  run: async () => null,
+  run: tip,
   getHelpMessage: async (msg) => ({
     embeds: [
       composeEmbedMessage(msg, {
@@ -27,7 +28,7 @@ const textCmd: Command = {
   }),
   actions,
   colorType: "Wallet",
-  canRunWithoutAction: false,
+  canRunWithoutAction: true,
   allowDM: true,
 }
 
