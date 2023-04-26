984b3bc2bab719f2c05e48da55129d93ca385141
haongo138 <haoxray@gmail.com>
2023-04-27 04:18:55 +0700

    chore: update test for tip parser

--- src/utils/tip-bot.test.ts
+++ src/utils/tip-bot.test.ts
@@ -1,5 +1,16 @@
 import config from "adapters/config"
-import { classifyTipSyntaxTargets, parseMonikerinCmd } from "./tip-bot"
+import {
+  classifyTipSyntaxTargets,
+  parseMonikerinCmd,
+  parseTipAmount,
+  parseMessageTip,
+  parseMoniker,
+} from "./tip-bot"
+import mockdc from "../../tests/mocks/discord"
+import { Message } from "discord.js"
+import { DiscordWalletTransferError } from "errors/discord-wallet-transfer"
+import { APIError } from "errors"
+jest.mock("adapters/config")
 
 describe("parseMonikerinCmd", () => {
   test("parse successful", async () => {
@@ -124,4 +135,202 @@ test.each([
   ["<:asd:642702608393568256>", { targets: [], isValid: false }],
 ])("defi.classifyTipSyntaxTargets(%o)", (input, output) => {
   expect(classifyTipSyntaxTargets(input)).toStrictEqual(output)
+})
+
+describe("parseTipAmount", () => {
+  let msg: Message
+
+  beforeEach(() => (msg = mockdc.cloneMessage()))
+  afterEach(() => jest.clearAllMocks())
+
+  test("Negative - negative amount", () => {
+    expect(() => parseTipAmount(msg, "-1")).toThrow(
+      new DiscordWalletTransferError({
+        discordId: msg.author.id,
+        message: msg,
+        error: "The amount is invalid. Please insert a positive number.",
+        title: "Invalid amount",
+      })
+    )
+  })
+
+  test("Negative - zero amount", () => {
+    expect(() => parseTipAmount(msg, "0")).toThrow(
+      new DiscordWalletTransferError({
+        discordId: msg.author.id,
+        message: msg,
+        error: "The amount is invalid. Please insert a positive number.",
+        title: "Invalid amount",
+      })
+    )
+  })
+
+  test("Negative - non-numeric amount", () => {
+    expect(() => parseTipAmount(msg, "x!")).toThrow(
+      new DiscordWalletTransferError({
+        discordId: msg.author.id,
+        message: msg,
+        error: "The amount is invalid. Please insert a positive number.",
+        title: "Invalid amount",
+      })
+    )
+  })
+
+  test("Positive - valid amount", () => {
+    const expected = {
+      all: false,
+      amount: 1,
+    }
+    const output = parseTipAmount(msg, "1")
+    expect(output).toStrictEqual(expected)
+  })
+
+  test("Positive - a / an amount", () => {
+    const expected = {
+      all: false,
+      amount: 1,
+    }
+    const firstOutput = parseTipAmount(msg, "a")
+    const secondOutput = parseTipAmount(msg, "an")
+    expect(firstOutput).toStrictEqual(expected)
+    expect(secondOutput).toStrictEqual(expected)
+  })
+
+  test("Positive - tip all", () => {
+    const expected = {
+      all: true,
+      amount: 0,
+    }
+    const output = parseTipAmount(msg, "all")
+    expect(output).toStrictEqual(expected)
+  })
+})
+
+describe("parseMessageTip", () => {
+  test("valid message with no quotes", () => {
+    const args = ["tip", "haongo", "1", "ftm", "a test message"]
+    const output = parseMessageTip(args, 4)
+    expect(output).toEqual("a test message")
+  })
+
+  test("valid message with single quotes", () => {
+    const args = ["tip", "haongo", "1", "ftm", "'a test message'"]
+    const output = parseMessageTip(args, 4)
+    expect(output).toEqual("a test message")
+  })
+
+  test("valid message with double quotes", () => {
+    const args = ["tip", "haongo", "1", "ftm", `"a test message"`]
+    const output = parseMessageTip(args, 4)
+    expect(output).toEqual("a test message")
+  })
+
+  test("valid message with space between", () => {
+    const args = ["tip", "haongo", "1", "ftm", "a", "test", "message"]
+    const output = parseMessageTip(args, 4)
+    expect(output).toEqual("a test message")
+  })
+})
+
+describe("parseMoniker", () => {
+  test("Negative - API error", async () => {
+    const input = {
+      unit: "coffee",
+      guildId: "test",
+    }
+    const apiRes = { ok: false, curl: "test", log: "test", data: [], error: "" }
+    config.getMonikerConfig = jest.fn().mockResolvedValueOnce(apiRes)
+    await expect(parseMoniker(input.unit, input.guildId)).rejects.toThrow(
+      new APIError({
+        description: apiRes.log,
+        curl: apiRes.curl,
+      })
+    )
+  })
+
+  test("Positive - Moniker found", async () => {
+    const input = {
+      unit: "coffee",
+      guildId: "test",
+    }
+    const monikerRes = {
+      ok: true,
+      error: null,
+      data: [
+        {
+          value: 0.4,
+          moniker: {
+            amount: 1,
+            moniker: "coffee",
+            plural: "coffee",
+            token: {
+              token_symbol: "ftm",
+            },
+          },
+        },
+      ],
+    }
+    const expected = {
+      value: 0.4,
+      moniker: {
+        amount: 1,
+        moniker: "coffee",
+        plural: "coffee",
+        token: {
+          token_symbol: "ftm",
+        },
+      },
+    }
+    config.getMonikerConfig = jest.fn().mockResolvedValueOnce(monikerRes)
+    const output = await parseMoniker(input.unit, input.guildId)
+    expect(output).toStrictEqual(expected)
+    expect(config.getDefaultMoniker).toHaveBeenCalledTimes(0)
+  })
+
+  test("Positive - Moniker not found -> use guild default moniker", async () => {
+    const input = {
+      unit: "coffee",
+      guildId: "test",
+    }
+    const monikerRes = {
+      ok: true,
+      error: null,
+      data: [],
+    }
+    const defaultMonikerRes = {
+      ok: true,
+      error: null,
+      data: [
+        {
+          value: 0.4,
+          moniker: {
+            amount: 1,
+            moniker: "coffee",
+            plural: "coffee",
+            token: {
+              token_symbol: "ftm",
+            },
+          },
+        },
+      ],
+    }
+    const expected = {
+      value: 0.4,
+      moniker: {
+        amount: 1,
+        moniker: "coffee",
+        plural: "coffee",
+        token: {
+          token_symbol: "ftm",
+        },
+      },
+    }
+    config.getMonikerConfig = jest.fn().mockResolvedValueOnce(monikerRes)
+    config.getDefaultMoniker = jest
+      .fn()
+      .mockResolvedValueOnce(defaultMonikerRes)
+    const output = await parseMoniker(input.unit, input.guildId)
+    expect(output).toStrictEqual(expected)
+    expect(config.getDefaultMoniker).toHaveBeenCalledTimes(1)
+  })
 })
