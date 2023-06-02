7aa1419b1b8c73f06d742e058d0b6ba46279ba19
tuanddd <vincenttuandao@gmail.com>
2023-06-02 15:55:29 +0700

    chore: remove deposit/withdraw text

--- src/commands/withdraw/index.ts
+++ src/commands/withdraw/index.ts
@@ -1,46 +1,20 @@
-import { Command, SlashCommand } from "types/common"
+import { SlashCommand } from "types/common"
 import {
   DEFI_DEFAULT_FOOTER,
   DEPOSIT_GITBOOK,
-  PREFIX,
   SLASH_PREFIX,
 } from "utils/constants"
 import { composeEmbedMessage } from "ui/discord/embed"
 import { thumbnails } from "utils/common"
 import { SlashCommandBuilder } from "@discordjs/builders"
-import withdraw from "./index/text"
 import withdrawSlash from "./index/slash"
-
-const textCmd: Command = {
-  id: "withdraw",
-  command: "withdraw",
-  brief: `Token withdrawal`,
-  category: "Defi",
-  run: withdraw,
-  getHelpMessage: async (msg) => {
-    const embedMsg = composeEmbedMessage(msg, {
-      description:
-        "Withdraw tokens to your wallet outside of Discord. A network fee will be added on top of your withdrawal (or deducted if remaining balance is insufficient).\nYou will be asked to confirm it.",
-      usage: `${PREFIX}withdraw <amount> <token>`,
-      examples: `${PREFIX}withdraw 5 ftm`,
-      document: DEPOSIT_GITBOOK,
-      footer: [DEFI_DEFAULT_FOOTER],
-    })
-    return { embeds: [embedMsg] }
-  },
-  canRunWithoutAction: true,
-  aliases: ["wd"],
-  colorType: "Defi",
-  allowDM: true,
-  minArguments: 3,
-}
 
 const slashCmd: SlashCommand = {
   name: "withdraw",
   category: "Defi",
-  prepare: () => {
+  prepare: (alias = "withdraw") => {
     return new SlashCommandBuilder()
-      .setName("withdraw")
+      .setName(alias)
       .setDescription("Withdraw tokens to your wallet outside of Discord.")
       .addStringOption((option) =>
         option
@@ -74,4 +48,4 @@ const slashCmd: SlashCommand = {
   colorType: "Defi",
 }
 
-export default { textCmd, slashCmd }
+export default { slashCmd }
