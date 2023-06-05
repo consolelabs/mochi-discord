3aa0f841165d2ec764c2892f85638e0c8a158c50
Minh Trần <thminhVN@users.noreply.github.com>
2023-06-05 16:49:52 +0700

    feat: earn new list (#1149)
    * feat: earn new list
    
    * chore: restructure earn command + integrate with router
    
    ---------
    
    Co-authored-by: tuanddd <vincenttuandao@gmail.com>

--- src/commands/earn/index.ts
+++ src/commands/earn/index.ts
@@ -1,22 +1,58 @@
-import { SlashCommandBuilder } from "@discordjs/builders"
+import {
+  SlashCommandBuilder,
+  SlashCommandSubcommandBuilder,
+} from "@discordjs/builders"
 import { SlashCommand } from "types/common"
 import { composeEmbedMessage } from "ui/discord/embed"
 import { SLASH_PREFIX } from "utils/constants"
+import { MachineConfig } from "utils/router"
+import airdrop from "./airdrop/slash"
+import quest from "./quest/slash"
+
+export const machineConfig: MachineConfig = {
+  id: "earn",
+  initial: "airdrops",
+  states: {
+    airdrops: {
+      on: {
+        VIEW_AIRDROP: "airdrop",
+        VIEW_QUEST: "quests",
+        PAGE: "airdrops",
+      },
+    },
+    airdrop: {
+      on: {
+        BACK: "airdrops",
+      },
+    },
+    quests: {
+      on: {
+        VIEW_AIRDROP: "airdrops",
+      },
+    },
+  },
+}
+
+const subCommands: Record<string, SlashCommand> = {
+  airdrop,
+  quest,
+}
 
 const slashCmd: SlashCommand = {
   name: "earn",
   category: "Defi",
   prepare: () => {
-    return new SlashCommandBuilder()
+    const data = new SlashCommandBuilder()
       .setName("earn")
-      .setDescription("view earning opportunies")
+      .setDescription(
+        "view earning opportunities, from airdrop campaigns to quests"
+      )
+
+    data.addSubcommand(<SlashCommandSubcommandBuilder>airdrop.prepare())
+    data.addSubcommand(<SlashCommandSubcommandBuilder>quest.prepare())
+    return data
   },
-  run: () =>
-    Promise.resolve({
-      messageOptions: {
-        content: "earn",
-      },
-    }),
+  run: (i) => subCommands[i.options.getSubcommand(true)]?.run(i),
   help: () =>
     Promise.resolve({
       embeds: [
