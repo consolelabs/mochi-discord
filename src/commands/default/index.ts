78007f58be664e927c2d16adaf59e289ff4b04ca
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2023-06-06 18:11:54 +0700

    feat: cmd default ticker (#1171)

---
+++ src/commands/default/index.ts
@@ -0,0 +1,59 @@
+import { SlashCommand } from "types/common"
+import { composeEmbedMessage } from "ui/discord/embed"
+import { LOG_CHANNEL_GITBOOK, SLASH_PREFIX } from "utils/constants"
+
+// slash
+import tickerSlash from "./ticker/slash"
+import {
+  SlashCommandBuilder,
+  SlashCommandSubcommandBuilder,
+} from "@discordjs/builders"
+import { CommandInteraction } from "discord.js"
+
+const slashActions: Record<string, SlashCommand> = {
+  ticker: tickerSlash,
+}
+
+const slashCmd: SlashCommand = {
+  name: "default",
+  category: "Config",
+  onlyAdministrator: function (i) {
+    const onlyAdmin = slashActions[i.options.getSubcommand()].onlyAdministrator
+    if (typeof onlyAdmin === "function") return onlyAdmin(i)
+    return Boolean(onlyAdmin)
+  },
+  autocomplete: function (i) {
+    const subCmd = i.options.getSubcommand()
+    slashActions[subCmd]?.autocomplete?.(i)
+    return Promise.resolve()
+  },
+  prepare: () => {
+    const data = new SlashCommandBuilder()
+      .setName("default")
+      .setDescription("Setup default aspect for your guild")
+    data.addSubcommand(<SlashCommandSubcommandBuilder>tickerSlash.prepare())
+    return data
+  },
+  run: function (interaction: CommandInteraction) {
+    return slashActions[interaction.options.getSubcommand()].run(interaction)
+  },
+  help: () =>
+    Promise.resolve({
+      embeds: [
+        composeEmbedMessage(null, {
+          includeCommandsList: true,
+          usage: `${SLASH_PREFIX}default <action>`,
+          description: "Setup default aspect for your guild",
+          footer: [
+            `Type ${SLASH_PREFIX}help default <action> for a specific action!`,
+          ],
+          document: LOG_CHANNEL_GITBOOK,
+          title: "Default",
+          examples: `${SLASH_PREFIX}default ticker ftm`,
+        }),
+      ],
+    }),
+  colorType: "Server",
+}
+
+export default { slashCmd }
---
+++ src/commands/default/ticker/processor.ts
@@ -0,0 +1,61 @@
+import config from "adapters/config"
+import { CommandInteraction } from "discord.js"
+import { GuildIdNotFoundError, InternalError } from "errors"
+import { getEmoji } from "utils/common"
+import CacheManager from "cache/node-cache"
+import defi from "adapters/defi"
+import { getSuccessEmbed } from "ui/discord/embed"
+import { getSlashCommand } from "utils/commands"
+
+export async function runDefaultTicker(interaction: CommandInteraction) {
+  if (!interaction.guildId) {
+    throw new GuildIdNotFoundError({ message: interaction })
+  }
+
+  // get symbol and validate if we support or not
+  const symbol = interaction.options.getString("symbol", true)
+  const default_ticker = interaction.options.getString("default", true)
+  const { data: coins } = await CacheManager.get({
+    pool: "ticker",
+    key: `ticker-search-${symbol}`,
+    call: () => defi.searchCoins(symbol, ""),
+  })
+  if (!coins || !coins.length) {
+    throw new InternalError({
+      title: "Unsupported token/fiat",
+      msgOrInteraction: interaction,
+      description: `**${symbol.toUpperCase()}** is invalid or hasn't been supported.\n${getEmoji(
+        "ANIMATED_POINTING_RIGHT",
+        true
+      )} Please choose a token that is listed on [CoinGecko](https://www.coingecko.com).\n${getEmoji(
+        "ANIMATED_POINTING_RIGHT",
+        true
+      )} or Please choose a valid fiat currency.`,
+    })
+  }
+
+  const { ok } = await config.setGuildDefaultTicker({
+    guild_id: interaction.guildId ?? "",
+    default_ticker,
+    query: symbol,
+  })
+  if (!ok) {
+    throw new InternalError({
+      title: "Cannot set default ticker",
+      msgOrInteraction: interaction,
+      description: `Please try again later.`,
+    })
+  }
+
+  const embeds = getSuccessEmbed({
+    description: `Next time your server members use ${await getSlashCommand(
+      "ticker"
+    )}, **${default_ticker}** will be the default selection.`,
+  })
+
+  return {
+    messageOptions: {
+      embeds: [embeds],
+    },
+  }
+}
