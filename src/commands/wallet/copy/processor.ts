5b67697355b8b59598f699af6a82653298409883
Bien Vo <vhbien000@gmail.com>
2023-06-06 21:16:13 +0700

    fix: update ui for cmd wallet track and copy when success (#1173)

--- src/commands/wallet/copy/processor.ts
+++ src/commands/wallet/copy/processor.ts
@@ -1,9 +1,16 @@
 import defi from "adapters/defi"
-import { BalanceType, renderBalances } from "commands/balances/index/processor"
-import { User } from "discord.js"
+import { User, MessageActionRow, MessageButton } from "discord.js"
 import { InternalError, OriginalMessage } from "errors"
-import { getEmoji, isAddress } from "utils/common"
+import {
+  getEmoji,
+  isAddress,
+  emojis,
+  getEmojiURL,
+  msgColors,
+  shortenHashOrAddress,
+} from "utils/common"
 import { WalletTrackingType } from ".."
+import { composeEmbedMessage } from "ui/discord/embed"
 
 export async function copyWallet(
   msg: OriginalMessage,
@@ -47,19 +54,71 @@ export async function copyWallet(
       description: "Couldn't copy wallet",
     })
   }
-  const { messageOptions } = await renderBalances(
-    author.id,
-    msg,
-    BalanceType.Onchain,
-    address
-  )
-
-  const iconURl = messageOptions.embeds[0].author?.iconURL
-  if (alias) {
-    messageOptions.embeds[0].setAuthor(alias, iconURl)
-  } else {
-    messageOptions.embeds[0].setAuthor("Wallet copied", iconURl)
-  }
+  const { messageOptions } = renderTrackingResult(msg, author.id, address)
 
   return messageOptions
+}
+
+function renderTrackingResult(
+  msg: OriginalMessage,
+  authorID: string,
+  address: string
+) {
+  return {
+    messageOptions: {
+      embeds: [
+        composeEmbedMessage(null, {
+          author: [
+            `${shortenHashOrAddress(
+              address
+            )} has been added to the watchlist to copy trades`,
+            getEmojiURL(emojis.CHECK),
+          ],
+          color: msgColors.SUCCESS,
+          description: `
+${getEmoji(
+  "ANIMATED_POINTING_RIGHT",
+  true
+)} View wallet watchlist by clicking on button \`Watchlist\`.
+${getEmoji(
+  "ANIMATED_POINTING_RIGHT",
+  true
+)} Follow this wallet by clicking on button \`Follow\`.
+${getEmoji(
+  "ANIMATED_POINTING_RIGHT",
+  true
+)} Track wallet's transactions by clicking on button \`Track\`.
+${getEmoji(
+  "ANIMATED_POINTING_RIGHT",
+  true
+)} To remove copy trade setting, click on button \`Uncopy\`.
+            `,
+        }),
+      ],
+      components: [
+        new MessageActionRow().addComponents(
+          new MessageButton()
+            .setLabel("Watchlist")
+            .setStyle("PRIMARY")
+            .setCustomId(`view_wallet`)
+            .setEmoji(emojis.PROPOSAL),
+          new MessageButton()
+            .setLabel("Follow")
+            .setStyle("SECONDARY")
+            .setCustomId(`follow_wallet-${authorID}-${address}`)
+            .setEmoji(emojis.PLUS),
+          new MessageButton()
+            .setLabel("Track")
+            .setStyle("SECONDARY")
+            .setCustomId(`track_wallet-${authorID}-${address}`)
+            .setEmoji(emojis.ANIMATED_STAR),
+          new MessageButton()
+            .setLabel("Uncopy")
+            .setStyle("SECONDARY")
+            .setCustomId(`uncopytrade_wallet-${authorID}-${address}`)
+            .setEmoji(emojis.REVOKE)
+        ),
+      ],
+    },
+  }
 }
--- src/commands/wallet/copy/slash.ts
+++ src/commands/wallet/copy/slash.ts
@@ -1,7 +1,36 @@
 import { SlashCommandSubcommandBuilder } from "@discordjs/builders"
-import { CommandInteraction } from "discord.js"
+import { CommandInteraction, Message } from "discord.js"
 import { SlashCommand } from "types/common"
 import { copyWallet } from "./processor"
+import { MachineConfig, route } from "utils/router"
+
+export const machineConfig: MachineConfig = {
+  id: "wallet-copy",
+  initial: "copy",
+  states: {
+    copy: {
+      on: {
+        VIEW_WALLET: "wallets",
+      },
+    },
+    wallets: {
+      id: "wallets",
+      initial: "wallets",
+      states: {
+        wallets: {
+          on: {
+            VIEW_WALLET: "wallet",
+          },
+        },
+        wallet: {
+          on: {
+            BACK: "wallets",
+          },
+        },
+      },
+    },
+  },
+}
 
 const command: SlashCommand = {
   name: "copy",
@@ -34,9 +63,16 @@ const command: SlashCommand = {
     const chain = i.options.getString("chain", false) ?? "eth"
     const alias = i.options.getString("alias", false) ?? ""
 
-    return {
-      messageOptions: await copyWallet(i, i.user, address, chain, alias),
-    }
+    const copytradeWalletResult = await copyWallet(
+      i,
+      i.user,
+      address,
+      chain,
+      alias
+    )
+    const reply = await i.editReply(copytradeWalletResult)
+
+    route(reply as Message, i.user, machineConfig)
   },
   help: () => Promise.resolve({}),
   colorType: "Defi",
