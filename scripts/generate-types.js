2e685793815857d119d9bf51a0fd43907f4d155f
Bien Vo <vhbien000@gmail.com>
2023-06-06 09:58:37 +0700

    chore: update type gen swagger and get by discord with avoiding amount (#1166)

--- scripts/generate-types.js
+++ scripts/generate-types.js
@@ -5,7 +5,7 @@ const path = require("path");
 generateApi({
   name: "api.ts",
   output: path.resolve(process.cwd(), "./src/types/"),
-  url: "https://develop-api.mochi.pod.town/swagger/doc.json",
+  url: `${process.env.API_SERVER_HOST}/swagger/doc.json`,
   generateClient: false,
   silent: true,
 });
