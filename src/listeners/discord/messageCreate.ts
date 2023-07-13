1f5a8ef48ba1390e766adeb0fd9e116d5b7752a8
tuanddd <vincenttuandao@gmail.com>
2023-07-13 09:50:58 +0700

    fix: profile id in normal message

--- src/listeners/discord/messageCreate.ts
+++ src/listeners/discord/messageCreate.ts
@@ -3,34 +3,45 @@ import { handlePrefixedCommand } from "commands"
 import { Message } from "discord.js"
 import tagme from "handlers/tagme"
 import { textCommandAsyncStore } from "utils/async-storages"
-import { PREFIX, VALID_BOOST_MESSAGE_TYPES } from "utils/constants"
+import {
+  MOCHI_PROFILE_API_BASE_URL,
+  PREFIX,
+  VALID_BOOST_MESSAGE_TYPES,
+} from "utils/constants"
 import { wrapError } from "utils/wrap-error"
 import { DiscordEvent } from "./index"
-import { getProfileIdByDiscord } from "../../utils/profile"
+import fetch from "node-fetch"
 
 export const handleNormalMessage = async (message: Message) => {
-  if (message.channel.type === "DM") return
+  try {
+    if (message.channel.type === "DM") return
 
-  const messageType = VALID_BOOST_MESSAGE_TYPES.includes(message.type) ? 9 : 1
+    const messageType = VALID_BOOST_MESSAGE_TYPES.includes(message.type) ? 9 : 1
 
-  const stickers = Array.from(message.stickers.values())
+    const stickers = Array.from(message.stickers.values())
 
-  const profileId = await getProfileIdByDiscord(message.author.id)
-  const body = {
-    author: {
-      id: message.author.id,
-      avatar: message.author.avatarURL(),
-      username: message.author.username,
-    },
-    guild_id: message.guildId,
-    channel_id: message.channelId,
-    timestamp: message.createdAt.toISOString(),
-    content: message.content,
-    sticker_items: stickers,
-    type: messageType,
+    const profileRes = await fetch(
+      `${MOCHI_PROFILE_API_BASE_URL}/profiles/get-by-discord/${message.author.id}?no-fetch-amount=true`
+    )
+    const profileId = (await profileRes.json())?.id
+    const body = {
+      author: {
+        id: message.author.id,
+        avatar: message.author.avatarURL(),
+        username: message.author.username,
+      },
+      guild_id: message.guildId,
+      channel_id: message.channelId,
+      timestamp: message.createdAt.toISOString(),
+      content: message.content,
+      sticker_items: stickers,
+      type: messageType,
+    }
+
+    await webhook.pushDiscordWebhook("messageCreate", body, profileId)
+  } catch (e) {
+    // swallow, intentional
   }
-
-  await webhook.pushDiscordWebhook("messageCreate", body, profileId)
 }
 
 const events: DiscordEvent<"messageCreate"> = {
@@ -50,8 +61,8 @@ const events: DiscordEvent<"messageCreate"> = {
         }),
       },
       () => {
-        // deny handling if author is bot or message is empty (new user join server)
         wrapError(message, async () => {
+          // deny handling if author is bot or message is empty (new user join server)
           if (
             message.author.bot ||
             (!message.content && !message.stickers.size)
