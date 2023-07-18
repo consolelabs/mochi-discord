e7c53c061b3096c3d6ce6ef0217fe3829446e60f
TuanD <vincenttuandao@gmail.com>
2023-07-18 15:00:18 +0700

    fix: message reaction add (#1374)

--- src/listeners/discord/messageReactionAdd.ts
+++ src/listeners/discord/messageReactionAdd.ts
@@ -1,18 +1,11 @@
-import {
-  Message,
-  MessageReaction,
-  User,
-  ChannelLogsQueryOptions,
-} from "discord.js"
+import { Message, MessageReaction, User } from "discord.js"
 import { DiscordEvent } from "."
-import config from "adapters/config"
 import webhook from "adapters/webhook"
-import { composeEmbedMessage, starboardEmbed } from "ui/discord/embed"
 import { getReactionIdentifier } from "utils/commands"
 import { wrapError } from "utils/wrap-error"
 import { eventAsyncStore } from "utils/async-storages"
 
-const handleRepostableMessageTracking = async (
+const handleMessageReactionAdd = async (
   reaction: MessageReaction,
   user: User,
   msg: Message
@@ -42,109 +35,7 @@ const handleRepostableMessageTracking = async (
     author_roles,
   }
 
-  const res = await webhook.pushDiscordWebhook("messageReactionAdd", body)
-
-  if (res?.data.reaction_type === "conversation") {
-    // loop through all channels in current guild to identify which channel contains conversation has origin_start_message_id
-    const channel = msg.guild?.channels.cache.find(
-      (c) =>
-        c.type == "GUILD_TEXT" &&
-        c.messages.cache.has(res.data.origin_start_message_id)
-    )
-
-    if (channel && channel.isText()) {
-      const startId = res.data.origin_start_message_id - 1
-      const options: ChannelLogsQueryOptions = {
-        after: startId.toString(),
-        before: res.data.origin_stop_message_id,
-      }
-
-      const rawConversation = await channel.messages.fetch(options)
-
-      // although ChannelLogsQueryOptions has before and after message id but the data is not correct 100%
-      // i need to get the start id and stop id out, then get msg which start id < is < stop id
-      const conversationArr: Message[] = []
-      for (msg of rawConversation.values()) {
-        conversationArr.push(msg)
-      }
-
-      const startMsg = conversationArr.filter(
-        (m) => m.id === res.data.origin_start_message_id
-      )
-
-      const stopMsg = conversationArr.filter(
-        (m) => m.id === res.data.origin_stop_message_id
-      )
-
-      const converstion = conversationArr
-        .filter((m) => {
-          return m.id >= startMsg[0].id && m.id <= stopMsg[0].id
-        })
-        .reverse()
-
-      let conversationMsg = ``
-      for (const msg of converstion.values()) {
-        conversationMsg += `${msg.author.username}: ${msg.content}\n`
-      }
-
-      // send conversation to sb channel
-      const repostChannel = msg.guild?.channels.cache.find(
-        (c) => c.id === res?.data.repost_channel_id
-      )
-      if (repostChannel && repostChannel.isText()) {
-        await repostChannel.send({
-          embeds: [
-            composeEmbedMessage(null, {
-              description: conversationMsg,
-            }),
-          ],
-        })
-      }
-    }
-  } else {
-    if (res?.data?.repost_channel_id) {
-      const { repost_channel_id: repostChannelId } = res.data
-      const { channel_id, guild_id, message_id, reaction, reaction_count } =
-        body
-
-      const channel = msg.guild?.channels.cache.find(
-        (c) => c.id === repostChannelId
-      )
-
-      const embed = starboardEmbed(msg)
-      if (channel) {
-        if (channel.isText()) {
-          // if repost message not exist, create one and store to db
-          if (!res?.data.repost_message_id) {
-            const sentMsg = await channel.send({
-              embeds: [embed],
-              content: `**${reaction} ${reaction_count}** <#${channel_id}>`,
-            })
-
-            config.editMessageRepost({
-              guild_id,
-              origin_message_id: message_id,
-              origin_channel_id: channel_id,
-              repost_channel_id: repostChannelId,
-              repost_message_id: sentMsg.id,
-            })
-          } else {
-            channel.messages
-              .fetch(`${res?.data.repost_message_id}`)
-              .then((msg) => {
-                msg
-                  .edit({
-                    embeds: [embed],
-                    content: `**${reaction} ${reaction_count}** <#${channel_id}>`,
-                  })
-                  .catch(() => null)
-              })
-              .catch(() => null)
-          }
-        }
-      }
-    }
-  }
+  await webhook.pushDiscordWebhook("messageReactionAdd", body)
 }
 
 const event: DiscordEvent<"messageReactionAdd"> = {
@@ -177,11 +68,7 @@ const event: DiscordEvent<"messageReactionAdd"> = {
                       if (user.bot) return
                       if (!msg.guild) return
 
-                      await handleRepostableMessageTracking(
-                        reaction,
-                        user,
-                        msg
-                      ).catch(() => null)
+                      await handleMessageReactionAdd(reaction, user, msg)
                     })
                   }
                 )
