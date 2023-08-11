0127dbe264d9837a1139bbd4ce1e1b7bba9b59bb
phucledien <phucledien@gmail.com>
2023-08-11 14:40:37 +0700

    fix: update format transaction

--- src/commands/transaction/index/processor.ts
+++ src/commands/transaction/index/processor.ts
@@ -1,18 +1,10 @@
-import {
-  emojis,
-  getEmoji,
-  getEmojiURL,
-  msgColors,
-  shortenHashOrAddress,
-} from "utils/common"
+import { emojis, getEmoji, getEmojiURL, msgColors } from "utils/common"
 import { APIError } from "errors"
 import { composeEmbedMessage } from "ui/discord/embed"
 import { CommandInteraction } from "discord.js"
 import profile from "adapters/profile"
 import mochiPay from "adapters/mochi-pay"
-import { convertString } from "../../../utils/convert"
-import { groupBy } from "lodash"
-import { fmt, utils } from "utils/formatter"
+import { fmt } from "utils/formatter"
 import { Platform } from "@consolelabs/mochi-formatter"
 
 export async function render(i: CommandInteraction) {
@@ -38,14 +30,17 @@ export async function render(i: CommandInteraction) {
       },
     }
 
-  const { data, ok, curl, error, log } = await mochiPay.getListTx(
-    dataProfile.id,
-    {}
-  )
+  const {
+    data: txns,
+    ok,
+    curl,
+    error,
+    log,
+  } = await mochiPay.getListTx(dataProfile.id, {})
   if (!ok) {
     throw new APIError({ curl, error, description: log })
   }
-  if (!data) {
+  if (!txns) {
     return {
       messageOptions: {
         embed: composeEmbedMessage(null, {
@@ -60,109 +55,16 @@ export async function render(i: CommandInteraction) {
     }
   }
 
-  const txnsGroupByAction = groupBy(
-    data.filter((tx: any) => tx.action !== "swap"), // filter out swap tx
-    "action"
-  )
-  const depositTxns = (txnsGroupByAction["deposit"] ?? []).slice(0, 10)
-  const withdrawTxns = (txnsGroupByAction["withdraw"] ?? []).slice(0, 10)
-  const tipTxns = (txnsGroupByAction["transfer"] ?? []).slice(0, 10)
-
-  const [dep, withdraw, tip] = await Promise.all([
-    // deposit
-    new Promise((r) => {
-      r(
-        utils.mdTable(
-          depositTxns.map((s: any) => ({
-            left: `+ ${utils.formatTokenDigit(
-              convertString(s.amount, s.token.decimal, false).toString()
-            )} ${s.token?.symbol?.toUpperCase() ?? "TOKEN"}`,
-            right: shortenHashOrAddress(s.from, 4) ?? "Unknown",
-          })),
-          { cols: ["left", "right"], wrapLastCol: false }
-        )
-      )
-    }),
-    // withdraw
-    new Promise((r) => {
-      r(
-        utils.mdTable(
-          withdrawTxns.map((s: any) => ({
-            left: `- ${utils.formatTokenDigit(
-              convertString(s.amount, s.token.decimal, false).toString()
-            )} ${s.token?.symbol?.toUpperCase() ?? "TOKEN"}`,
-            right: shortenHashOrAddress(s.address, 4) ?? "Unknown",
-          })),
-          { cols: ["left", "right"], wrapLastCol: false }
-        )
-      )
-    }),
-    // tip
-    new Promise((resolve) => {
-      Promise.all(
-        tipTxns.map(async (tx: any) => {
-          const txRow = {
-            left: `${tx.type === "out" ? "-" : "+"} ${utils.formatTokenDigit(
-              convertString(tx.amount, tx.token.decimal, false).toString()
-            )} ${tx.token?.symbol?.toUpperCase() ?? "TOKEN"}`,
-            right: "someone",
-          }
-          const [targetUser] = await fmt.account(
-            Platform.Discord,
-            tx.other_profile_id
-          )
-          if (!targetUser) {
-            return txRow
-          }
-          txRow.right = targetUser.value
-          return txRow
-        })
-      ).then((txRow) => {
-        resolve(
-          utils.mdTable(txRow, {
-            cols: ["left", "right"],
-            wrapLastCol: false,
-            alignment: ["left", "left"],
-          })
-        )
-      })
-    }),
-  ])
-
-  const fields = [
-    ...(tipTxns.length
-      ? [
-          {
-            name: "Tip",
-            value: `${tip}`,
-            inline: false,
-          },
-        ]
-      : []),
-    ...(depositTxns.length
-      ? [
-          {
-            name: "Deposit",
-            value: `${dep}`,
-            inline: false,
-          },
-        ]
-      : []),
-    ...(withdrawTxns.length
-      ? [
-          {
-            name: "Withdraw",
-            value: `${withdraw}`,
-            inline: false,
-          },
-        ]
-      : []),
-  ]
+  const { text: description } = await fmt.components.txns({
+    txns: txns as any,
+    on: Platform.Discord,
+    groupDate: true,
+  })
 
   const embed = composeEmbedMessage(null, {
     color: msgColors.BLUE,
     author: ["Transactions", getEmojiURL(emojis.TRANSACTIONS)],
-  }).addFields(fields)
-
+    description,
+  })
   return { messageOptions: { embeds: [embed] } }
 }
