d810ecec72aa0272505feccbd5add0a4d46174ce
TuanD <vincenttuandao@gmail.com>
2023-08-17 17:13:16 +0700

    chore: use new version formatter (#1448)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,10 +1,11 @@
 import Formatter, { utils } from "@consolelabs/mochi-formatter"
 import Redis from "ioredis"
 import { REDIS_DB, REDIS_HOST } from "env"
-import { MOCHI_PROFILE_API_BASE_URL } from "./constants"
+import { HOMEPAGE_URL, MOCHI_PROFILE_API_BASE_URL } from "./constants"
 
 Formatter.profileApi = MOCHI_PROFILE_API_BASE_URL
 
 const redis = new Redis(`redis://${REDIS_HOST}/${REDIS_DB}`)
 Formatter.redis = redis
+Formatter.homepageUrl = HOMEPAGE_URL
 export { Formatter as fmt, utils }
