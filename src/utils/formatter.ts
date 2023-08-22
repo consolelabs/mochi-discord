ee3722b2bc7ed07691f46e5751f7e64bd03a6b8e
tuanddd <vincenttuandao@gmail.com>
2023-08-22 08:08:09 +0700

    fix: add back homepageUrl to formatter

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,12 +1,16 @@
 import Formatter, { utils } from "@consolelabs/mochi-formatter"
 import Redis from "ioredis"
 import { REDIS_DB, REDIS_HOST } from "env"
-import { MOCHI_PROFILE_API_BASE_URL, API_BASE_URL } from "./constants"
+import {
+  MOCHI_PROFILE_API_BASE_URL,
+  API_BASE_URL,
+  HOMEPAGE_URL,
+} from "./constants"
 
 Formatter.profileApi = MOCHI_PROFILE_API_BASE_URL
 Formatter.baseApi = API_BASE_URL
 
 const redis = new Redis(`redis://${REDIS_HOST}/${REDIS_DB}`)
 Formatter.redis = redis
-// Formatter.homepageUrl = HOMEPAGE_URL
+Formatter.homepageUrl = HOMEPAGE_URL
 export { Formatter as fmt, utils }
