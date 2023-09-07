410d851823517cb99b0ad61e867b3efad6b3fb30
tuanddd <vincenttuandao@gmail.com>
2023-09-07 19:43:18 +0700

    feat(v): add command /v to check the bot's current version

---
+++ src/commands/v/index.ts
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder } from "@discordjs/builders"
+import { SlashCommand } from "types/common"
+import { version } from "../../../package.json"
+
+const slashCmd: SlashCommand = {
+  name: "v",
+  category: "Config",
+  prepare: () => {
+    return new SlashCommandBuilder()
+      .setName("v")
+      .setDescription("Check bot's version")
+  },
+  run: async function () {
+    return {
+      messageOptions: {
+        content: `v${version} - ${process.env.NODE_ENV}`,
+      },
+    }
+  },
+  help: () => Promise.resolve({}),
+  colorType: "Server",
+}
+
+export default { slashCmd }
