fa1d95bcf7427b5bd1f66050cee66b77b7d69bb9
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2023-09-12 15:34:32 +0700

    fix(tip): use animated version of chat emoji

--- src/commands/tip/index/processor.ts
+++ src/commands/tip/index/processor.ts
@@ -290,7 +290,7 @@ function showSuccesfulResponse(
     description += `\n${getEmoji("ANIMATED_ROBOT", true)}\`Message.  \`${
       payload.message
     }`
-    contentMsg += `\n${getEmoji("CHAT", true)}\` Message. \`${payload.message}`
+    contentMsg += `\n${getEmoji("ANIMATED_CHAT", true)}\`Message. \`${payload.message}`
   }
   const embed = composeEmbedMessage(null, {
     author: [
