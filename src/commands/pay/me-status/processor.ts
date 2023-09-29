5a8fe83123631c4c54e922fab352f2a9b7ecbf87
NguyenHuy1812 <nguyenhuy1812@gmail.com>
2023-09-29 13:38:50 +0700

    feat(pay): add payme and paylink status list cmd (#1503)

---
+++ src/commands/pay/me-status/processor.ts
@@ -0,0 +1,45 @@
+import { getAuthor } from "utils/common"
+import { paginationButtons } from "utils/router"
+import { CommandInteraction } from "discord.js"
+import UI, { PageSize, Platform } from "@consolelabs/mochi-ui"
+import mochiPay from "adapters/mochi-pay"
+import profile from "adapters/profile"
+import { APIError } from "errors"
+import { reply } from "utils/discord"
+import api from "api"
+
+export async function render(i: CommandInteraction, page = 0) {
+  const author = getAuthor(i)
+  // get profile id
+  const p = await profile.getByDiscord(author.id)
+  if (p.err) {
+    throw new APIError({
+      msgOrInteraction: i,
+      description: `[getByDiscord] API error with status ${p.status_code}`,
+      curl: "",
+    })
+  }
+
+  const payMes = await mochiPay.getPaymes(p.id)
+  const total = Math.ceil(payMes.length / PageSize.Medium)
+  const { text, totalPage } = await UI.components.payMes({
+    payMes,
+    on: Platform.Discord,
+    page,
+    total,
+    withTitle: true,
+    groupDate: true,
+    api,
+  })
+
+  const lines = []
+  lines.push(text)
+  lines.push("⎯".repeat(5))
+  lines.push("\\✅ Paid \\| \\⚠️ End soon \\| \\🔵 New \\| \\⚪ Expired")
+
+  return reply(i, {
+    messageOptions: {
+      content: lines.join("\n"),
+    },
+  })
+}
