a871d25c56270cb6ae5cf5b571c4374e3a249242
Le Dien Phuc <phucledien@gmail.com>
2023-10-03 09:56:31 +0700

    fix(gm): brings gm back (#1508)

--- src/listeners/discord/messageCreate.ts
+++ src/listeners/discord/messageCreate.ts
@@ -1,9 +1,48 @@
+import webhook from "adapters/webhook"
 import { handlePrefixedCommand } from "commands"
+import { Message } from "discord.js"
 import tagme from "handlers/tagme"
 import { textCommandAsyncStore } from "utils/async-storages"
-import { PREFIX } from "utils/constants"
+import {
+  MOCHI_PROFILE_API_BASE_URL,
+  PREFIX,
+  VALID_BOOST_MESSAGE_TYPES,
+} from "utils/constants"
 import { wrapError } from "utils/wrap-error"
 import { DiscordEvent } from "./index"
+import fetch from "node-fetch"
+
+export const handleNormalMessage = async (message: Message) => {
+  try {
+    if (message.channel.type === "DM") return
+
+    const messageType = VALID_BOOST_MESSAGE_TYPES.includes(message.type) ? 9 : 1
+
+    const stickers = Array.from(message.stickers.values())
+
+    const profileRes = await fetch(
+      `${MOCHI_PROFILE_API_BASE_URL}/profiles/get-by-discord/${message.author.id}?no_fetch_amount=true`,
+    )
+    const profileId = (await profileRes.json())?.id
+    const body = {
+      author: {
+        id: message.author.id,
+        avatar: message.author.avatarURL(),
+        username: message.author.username,
+      },
+      guild_id: message.guildId,
+      channel_id: message.channelId,
+      timestamp: message.createdAt.toISOString(),
+      content: message.content,
+      sticker_items: stickers,
+      type: messageType,
+    }
+
+    await webhook.pushDiscordWebhook("messageCreate", body, profileId)
+  } catch (e) {
+    // swallow, intentional
+  }
+}
 
 const events: DiscordEvent<"messageCreate"> = {
   name: "messageCreate",
@@ -34,6 +73,7 @@ const events: DiscordEvent<"messageCreate"> = {
             return
           }
           tagme.handle(message)
+          await handleNormalMessage(message)
         })
       },
     )
