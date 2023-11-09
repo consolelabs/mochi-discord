a96b62a9b9f496766a6f37be424f3e914fa4a57f
Quang Le <quanglm.ops@gmail.com>
2023-11-09 16:19:15 +0700

    chore: remove unused pkg

--- src/adapters/defi.ts
+++ src/adapters/defi.ts
@@ -19,7 +19,6 @@ import {
   INDEXER_API_BASE_URL,
   POLYGONSCAN_API,
 } from "utils/constants"
-import { logger } from "logger"
 import { Fetcher } from "./fetcher"
 
 class Defi extends Fetcher {
