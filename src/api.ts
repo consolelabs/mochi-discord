91e1844f8de6917a4f1ce9e79c848df0b23c6b6e
anhnh12 <nguyenhoanganh0772@gmail.com>
2023-11-16 22:11:44 +0700

    fix: mochi-rest auth (#1523)

--- src/api.ts
+++ src/api.ts
@@ -3,10 +3,11 @@ import {
   MOCHI_PAY_API_SERVER_HOST,
   API_SERVER_HOST,
   MOCHI_PROFILE_API_SERVER_HOST,
+  MOCHI_BOT_SECRET,
 } from "env"
 import { logger } from "logger"
 
-export default new API({
+const api = new API({
   payUrl: `${MOCHI_PAY_API_SERVER_HOST}/api/v1`,
   baseUrl: `${API_SERVER_HOST}/api/v1`,
   profileUrl: `${MOCHI_PROFILE_API_SERVER_HOST}/api/v1`,
@@ -14,3 +15,6 @@ export default new API({
     logger.error(e)
   },
 })
+api.token(`Bearer ${MOCHI_BOT_SECRET}`)
+
+export default api
