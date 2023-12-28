f6968f3f5677368acaf1b628f438ce6a0d3125d0
Le Dien Phuc <phucledien@gmail.com>
2023-12-28 15:02:07 +0700

    fix: update wlv sort pagination logic (#1562)

--- src/commands/watchlist/view/processor.ts
+++ src/commands/watchlist/view/processor.ts
@@ -5,12 +5,14 @@ import defi from "adapters/defi"
 import community from "adapters/community"
 import { getSlashCommand } from "utils/commands"
 import { ResponseGetWatchlistResponse } from "types/api"
-import { groupBy } from "lodash"
+import { chunk, groupBy } from "lodash"
 import { getProfileIdByDiscord } from "../../../utils/profile"
 import { utils } from "@consolelabs/mochi-formatter"
 import { VERTICAL_BAR } from "utils/constants"
 import { renderChart } from "./chart"
 import { paginationButtons } from "utils/router"
+import { somethingWentWrongPayload } from "utils/error"
+import { APIError } from "errors"
 
 export function buildSwitchViewActionRow(currentView: string) {
   const tokenButton = new MessageButton({
@@ -65,26 +67,33 @@ export async function composeWatchlist(
   tokenView = WatchListTokenViewType.Text,
   user: User = author,
 ) {
+  switch (view) {
+    case WatchListViewType.Nft:
+      return await composeNFTWatchlist(author, page, user)
+    case WatchListViewType.Token:
+      return await composeTokenWatchlist(author, page, tokenView, user)
+  }
+}
+
+async function composeTokenWatchlist(
+  author: User,
+  page = 0,
+  tokenView = WatchListTokenViewType.Text,
+  user: User = author,
+) {
   const profileId = await getProfileIdByDiscord(user.id)
-  let data = []
-  let promise
-  if (view === WatchListViewType.Token) {
-    promise = defi.getUserWatchlist({
-      profileId,
-      page,
-      size: PAGE_SIZE,
-    })
-  } else {
-    promise = defi.getUserNFTWatchlist({
-      profileId,
-      page,
-      size: PAGE_SIZE,
-    })
+  const {
+    data: res,
+    ok,
+    error,
+    status = 500,
+    curl,
+    log,
+  } = await defi.getUserWatchlist({ profileId })
+  if (!ok) {
+    throw new APIError({ error, status, description: log, curl })
   }
-  const { data: res, ok } = await promise
-  if (ok) {
-    data = res.data
-  }
+  let data = res.data
   if (author.id) {
     await community.updateQuestProgress({
       userId: author.id,
@@ -109,65 +118,67 @@ export async function composeWatchlist(
     return {
       msgOpts: {
         embeds: [embed],
-        components: [buildSwitchViewActionRow(view)],
+        components: [buildSwitchViewActionRow(WatchListViewType.Token)],
       },
     }
   }
   let files: any[] = []
-  switch (view) {
-    case WatchListViewType.Nft:
-      // TODO
-      embed.setDescription("NFT WIP")
-      break
-    default:
-    case WatchListViewType.Token:
-      {
-        let tokenData = (data as ResponseGetWatchlistResponse["data"]) ?? []
-        if (tokenView === WatchListTokenViewType.Text) {
-          const group = groupBy(tokenData, (t) =>
-            Math.sign(t.price_change_percentage_24h ?? 0),
-          )
-          group[1] = group[1]?.sort(sortPrice(1)) ?? []
-          group["-1"] = group["-1"]?.sort(sortPrice(-1)) ?? []
+  // Pagination from frontend side, since we need to sort
+  let tokenData = (data as ResponseGetWatchlistResponse["data"]) ?? []
+  tokenData = tokenData.sort(
+    (a, b) =>
+      (b.price_change_percentage_24h ?? 0) -
+      (a.price_change_percentage_24h ?? 0),
+  )
+  let chunkedTokenData = chunk(
+    (data as ResponseGetWatchlistResponse["data"]) ?? [],
+    PAGE_SIZE,
+  )
+  let totalPage = chunkedTokenData.length
+  if (page >= totalPage) {
+    return {
+      msgOpts: {
+        ...somethingWentWrongPayload(),
+      },
+    }
+  }
+  tokenData = chunkedTokenData[page]
 
-          tokenData = [...group[1], ...(group[0] ?? []), ...group[-1]]
-          const { segments } = formatDataTable(
-            tokenData.map((t) => {
-              const price = t.current_price ?? 0
-              const priceChange = t.price_change_percentage_24h ?? 0
-              return {
-                symbol: (t.symbol ?? "").toUpperCase(),
-                priceChange: utils.formatPercentDigit(priceChange),
-                usd: utils.formatUsdPriceDigit(price),
-              }
-            }),
-            {
-              cols: ["symbol", "usd", "priceChange"],
-              separator: [VERTICAL_BAR, VERTICAL_BAR],
-              rowAfterFormatter: (f, i) =>
-                `${getEmojiToken(
-                  (tokenData[i].symbol ?? "") as TokenEmojiKey,
-                )}${f}${getEmoji(
-                  (tokenData[i].price_change_percentage_24h ?? 0) < 0
-                    ? "ARROW_DOWN"
-                    : "ARROW_UP",
-                )}`,
-            },
-          )
-
-          embed.setDescription(
-            `**${getEmoji(
-              "CHART",
-            )} All changes are in D1, sorted by price change**\n\n${segments
-              .map((c) => c.join("\n"))
-              .join("\n")}`,
-          )
-        } else {
-          embed.setImage("attachment://watchlist.png")
-          files = [await renderChart(data)]
+  if (tokenView === WatchListTokenViewType.Text) {
+    const { segments } = formatDataTable(
+      tokenData.map((t) => {
+        const price = t.current_price ?? 0
+        const priceChange = t.price_change_percentage_24h ?? 0
+        return {
+          symbol: (t.symbol ?? "").toUpperCase(),
+          priceChange: utils.formatPercentDigit(priceChange),
+          usd: utils.formatUsdPriceDigit(price),
         }
-      }
-      break
+      }),
+      {
+        cols: ["symbol", "usd", "priceChange"],
+        separator: [VERTICAL_BAR, VERTICAL_BAR],
+        rowAfterFormatter: (f, i) =>
+          `${getEmojiToken(
+            (tokenData[i].symbol ?? "") as TokenEmojiKey,
+          )}${f}${getEmoji(
+            (tokenData[i].price_change_percentage_24h ?? 0) < 0
+              ? "ARROW_DOWN"
+              : "ARROW_UP",
+          )}`,
+      },
+    )
+
+    embed.setDescription(
+      `**${getEmoji(
+        "CHART",
+      )} All changes are in D1, sorted by price change**\n\n${segments
+        .map((c) => c.join("\n"))
+        .join("\n")}`,
+    )
+  } else {
+    embed.setImage("attachment://watchlist.png")
+    files = [await renderChart(tokenData)]
   }
 
   return {
@@ -177,13 +188,34 @@ export async function composeWatchlist(
     msgOpts: {
       embeds: [embed],
       components: [
-        buildSwitchViewActionRow(view),
-        ...paginationButtons(
-          page,
-          Math.ceil((res.metadata?.total || 0) / PAGE_SIZE),
-        ),
+        buildSwitchViewActionRow(WatchListViewType.Token),
+        ...paginationButtons(page, totalPage),
       ],
       files,
+    },
+  }
+}
+
+async function composeNFTWatchlist(
+  author: User,
+  page = 0,
+  user: User = author,
+) {
+  const embed = composeEmbedMessage(null, {
+    author: [
+      `${user.username}'s watchlist`,
+      user.displayAvatarURL({ format: "png" }),
+    ],
+    description: "NFT WIP",
+  })
+
+  return {
+    context: {
+      page,
+    },
+    msgOpts: {
+      embeds: [embed],
+      components: [buildSwitchViewActionRow(WatchListViewType.Nft)],
     },
   }
 }
