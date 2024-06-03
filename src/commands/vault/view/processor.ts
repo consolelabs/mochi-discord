481a4f611bae3ce977b165a85bc5eec199c42897
Anh Nguyen <nguyenhoanganh0772@gmail.com>
2024-06-03 16:57:06 +0700

    feat: trading vault config view

--- src/commands/vault/view/processor.ts
+++ src/commands/vault/view/processor.ts
@@ -4,14 +4,48 @@ import { GuildIdNotFoundError } from "errors"
 import { composeEmbedMessage } from "ui/discord/embed"
 import { getEmoji, msgColors } from "utils/common"
 import { buildTreasurerFields } from "../info/processor"
+import mochiPay from "adapters/mochi-pay"
+import { getDiscordIdByProfileId, getProfileIdByDiscord } from "utils/profile"
 
 export async function run({
   i,
   guildId,
+  vaultId,
+  vaultType,
 }: {
   i: CommandInteraction
   guildId?: string | null
+  vaultId: string
+  vaultType: string
 }) {
+  if (vaultType === "trading") {
+    const profileId = await getProfileIdByDiscord(i.user.id)
+    const data = await mochiPay.getEarningVaultConfigs(profileId, vaultId)
+    const { name, api_key, secret_key, thresh_hold } = data
+    const basicInfo = [
+      `${getEmoji("ANIMATED_VAULT", true)}\`Name.        ${name}\``,
+      `${getEmoji("ANIMATED_VAULT_KEY")}\`API Key.    \` ${api_key}`,
+      `${getEmoji("ANIMATED_VAULT_KEY")}\`Secret Key. \` ${secret_key}`,
+      `${getEmoji("APPROVE")}\`Threshold.  \` ${thresh_hold}%`,
+      // `${getEmoji("NEWS")}\`Channel. \` <#1019524376527372288>`,
+    ].join("\n")
+
+    const treasurers = await Promise.all(
+      data.treasurers.map(async (t: any) => {
+        const discordId = await getDiscordIdByProfileId(t.profile_id)
+        return { user_discord_id: discordId, role: t.role }
+      }),
+    )
+
+    const treasurersFields = buildTreasurerFields({ treasurer: treasurers })
+    const embed = composeEmbedMessage(null, {
+      title: `${getEmoji("ANIMATED_DIAMOND")} Vault config`,
+      description: basicInfo,
+      color: msgColors.BLUE,
+    }).addFields(treasurersFields)
+
+    return { messageOptions: { embeds: [embed] } }
+  }
   if (!guildId) {
     throw new GuildIdNotFoundError({ message: i })
   }
