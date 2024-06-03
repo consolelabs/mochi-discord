11d8cd5b80d3d6bd88716928d541aa9fcd7c1d79
Anh Nguyen <nguyenhoanganh0772@gmail.com>
2024-06-03 10:43:14 +0700

    feat: vault commands flow

--- src/commands/vault/view/slash.ts
+++ src/commands/vault/view/slash.ts
@@ -5,6 +5,8 @@ import { SLASH_PREFIX } from "utils/constants"
 import { SlashCommand } from "types/common"
 import { run } from "./processor"
 import config from "adapters/config"
+import profile from "adapters/profile"
+import mochiPay from "adapters/mochi-pay"
 
 const command: SlashCommand = {
   name: "view",
@@ -28,15 +30,28 @@ const command: SlashCommand = {
       return
     }
     const focusedValue = i.options.getFocused()
-    const data = await config.vaultList(i.guildId, true)
+    // const data = await config.vaultList(i.guildId, true)
 
-    await i.respond(
-      data
+    const userProfile = await profile.getByDiscord(i.user.id)
+    const [spotVaults, tradingVaults] = await Promise.all([
+      config.vaultList(i.guildId, true),
+      mochiPay.listEarningVaults(userProfile.id),
+    ])
+
+    const options = [
+      ...spotVaults
         .filter((d: any) =>
           d.name.toLowerCase().includes(focusedValue.toLowerCase()),
         )
-        .map((d: any) => ({ name: d.name, value: d.name })),
-    )
+        .map((d: any) => ({ name: d.name, value: `spot_${d.name}` })),
+      ...tradingVaults
+        .filter((v: any) =>
+          v.name.toLowerCase().includes(focusedValue.toLowerCase()),
+        )
+        .map((v: any) => ({ name: v.name, value: `trading_${v.id}` })),
+    ]
+
+    await i.respond(options)
   },
   run: async function (interaction: CommandInteraction) {
     return run({
