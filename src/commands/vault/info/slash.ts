cac6f2c36708e8ca4df9c8fb114cac3a5e8d0d13
anhnh12 <nguyenhoanganh0772@gmail.com>
2024-08-01 17:07:03 +0700

    feat: trading vault deposit (#1698)

--- src/commands/vault/info/slash.ts
+++ src/commands/vault/info/slash.ts
@@ -3,17 +3,28 @@ import {
   depositDetail,
   renderListDepositAddress,
 } from "commands/deposit/index/processor"
-import { CommandInteraction, Message } from "discord.js"
+import {
+  ButtonInteraction,
+  CommandInteraction,
+  Message,
+  MessageActionRow,
+  MessageButton,
+  Modal,
+  TextInputComponent,
+} from "discord.js"
 import { SlashCommand } from "types/common"
 import { composeEmbedMessage } from "ui/discord/embed"
 import { chainTypes } from "utils/chain"
-import { equalIgnoreCase } from "utils/common"
+import { equalIgnoreCase, getEmoji, TokenEmojiKey } from "utils/common"
 import { GM_GITBOOK, SLASH_PREFIX } from "utils/constants"
 import { MachineConfig, route, RouterSpecialAction } from "utils/router"
 
 import { SlashCommandSubcommandBuilder } from "@discordjs/builders"
 
 import {
+  depositStep1,
+  depositStep2,
+  executeTradingVaultDeposit,
   handleVaultRounds,
   runGetVaultDetail,
   vaultClaim,
@@ -79,6 +90,7 @@ export const machineConfig: (
     },
     ephemeral: {
       DEPOSIT: true,
+      TRADING_VAULT_DEPOSIT: true,
     },
     ...context,
   },
@@ -109,6 +121,12 @@ export const machineConfig: (
         ROUNDS: "vaultRounds",
         REPORT: "vaultReport",
         CLAIM: "vaultClaim",
+        TRADING_VAULT_DEPOSIT: {
+          target: "vaultInfo",
+          actions: {
+            type: "tradingVaultDeposit",
+          },
+        },
         DEPOSIT: {
           target: "vaultInfo",
           actions: {
@@ -138,9 +156,10 @@ const command: SlashCommand = {
   autocomplete: async function (i) {
     const focusedValue = i.options.getFocused()
     const userProfile = await profile.getByDiscord(i.user.id)
-    const [spotVaults, tradingVaults] = await Promise.all([
+    const [spotVaults, tradingVaults, publicVaults] = await Promise.all([
       i.guildId ? config.vaultList(i.guildId, true) : [],
       i.guildId ? mochiPay.listEarningVaults(userProfile.id, i.guildId) : [],
+      await mochiPay.listGlobalEarningVault(userProfile.id),
     ])
 
     const options = [
@@ -150,6 +169,11 @@ const command: SlashCommand = {
         )
         .map((d: any) => ({ name: d.name, value: `spot_${d.name}` })),
       ...tradingVaults
+        .filter((v: any) =>
+          v.name.toLowerCase().includes(focusedValue.toLowerCase()),
+        )
+        .map((v: any) => ({ name: v.name, value: `trading_${v.id}` })),
+      ...publicVaults
         .filter((v: any) =>
           v.name.toLowerCase().includes(focusedValue.toLowerCase()),
         )
@@ -246,6 +270,101 @@ const command: SlashCommand = {
                   on: {
                     BACK: "depositList",
                   },
+                },
+              },
+            })
+          },
+          tradingVaultDeposit: async (_, event) => {
+            const i = event.interaction as ButtonInteraction
+            const { msgOpts, context: ctx } = await depositStep1(i, context)
+            const reply = (await i.editReply(msgOpts)) as Message
+
+            route(reply, i, {
+              id: "tradingvaultdeposit",
+              initial: "depositStep1",
+              context: {
+                modal: {
+                  ENTER_AMOUNT: true,
+                },
+                ephemeral: {
+                  ENTER_AMOUNT: true,
+                },
+                select: {
+                  depositStep2: (i, _ev, ctx) =>
+                    depositStep2(i, {
+                      ...(ctx as any),
+                      amount: ctx.depositAmount,
+                      tokenSymbol: i.values[0] as TokenEmojiKey,
+                    }),
+                },
+                button: {
+                  depositStep1: (i, _ev, ctx) => depositStep1(i, ctx),
+                  depositStep2: async (i, ev, ctx) => {
+                    if (ev === "ENTER_AMOUNT") {
+                      const modal = new Modal()
+                        .setCustomId("amount-form")
+                        .setTitle("Amount")
+                        .setComponents(
+                          new MessageActionRow<any>().setComponents([
+                            new TextInputComponent()
+                              .setCustomId("custom_amount")
+                              .setLabel("Value")
+                              .setStyle("SHORT")
+                              .setRequired(true),
+                          ]),
+                        )
+
+                      await i.showModal(modal)
+                      const submitted = await i
+                        .awaitModalSubmit({
+                          time: 300000,
+                          filter: (mi) => mi.user.id === i.user.id,
+                        })
+                        .catch(() => null)
+
+                      if (!submitted)
+                        return {
+                          msgOpts: {
+                            ...(i.message as Message),
+                            attachments: undefined,
+                          },
+                        }
+
+                      if (!submitted.deferred) {
+                        await submitted.deferUpdate().catch(() => null)
+                      }
+
+                      const amount =
+                        submitted.fields.getTextInputValue("custom_amount")
+                      return depositStep2(i, { ...(ctx as any), amount })
+                    }
+                    return depositStep2(i, {
+                      ...(ctx as any),
+                      amount: `%${ev.split("_").at(-1)}`,
+                    })
+                  },
+                  submit: (i, _ev, ctx) => executeTradingVaultDeposit(i, ctx),
+                },
+                ...ctx,
+              },
+              states: {
+                depositStep1: {
+                  on: {
+                    SELECT_TOKEN: "depositStep2",
+                  },
+                },
+                depositStep2: {
+                  on: {
+                    SELECT_AMOUNT_10: "depositStep2",
+                    SELECT_AMOUNT_25: "depositStep2",
+                    SELECT_AMOUNT_50: "depositStep2",
+                    SELECT_AMOUNT_100: "depositStep2",
+                    ENTER_AMOUNT: "depositStep2",
+                    SUBMIT: "submit",
+                  },
+                },
+                submit: {
+                  type: "final",
                 },
               },
             })
